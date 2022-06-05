de2c2d0d48f5293fa9ae457885eb8b37665ac4c2
Avila Diego <aviladiego2009@gmail.com>
2022-06-05 13:07:43 -0300

    boilerplate(api): Add boilerplate of API

---
+++ api/src/db.ts
@@ -0,0 +1,13 @@
+import {Sequelize} from 'sequelize-typescript';
+import config from '../config';
+config;
+
+
+export const sequelize = new Sequelize({
+ dialect: 'postgres',
+ database: config.dbName,
+ password: config.dbPassword,
+ username: config.dbUser,
+ storage: ':memory:',
+ models: [__dirname + '/models'],
+});
