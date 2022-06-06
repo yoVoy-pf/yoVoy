3998bf207d60544a3725c84a8fa7e76ce12fb382
Mariano Dunand <marianoo.14.md@gmail.com>
2022-06-06 18:29:51 -0300

    refactor(client/home) : Event component is rendered, and Event interface is now imported

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,48 +1,29 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getAllEvent } from "../../redux/actions/actions-Create";
-import { AppDispatch, State } from "../../redux/store/store";
-import Card from "../Card/Card";
-import SearchBar from '../SearchBar/SearchBar'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllEvent } from '../../redux/actions/actions-Create';
+import { AppDispatch, State } from '../../redux/store/store';
+import Card from '../Card/Card';
+import Events from '../Events/Events';
+import SearchBar from '../SearchBar/SearchBar';
+
 import s from './home.module.css';
 
-interface Event {
-    name: string;
-    image: string;
-    location: string;
-    place: string;
-    date: string;
-}
-
 const Home = () => {
-    
-    const dispatch: AppDispatch = useDispatch();
-    const allEvents = useSelector((state: State) => state.allEvents)
+	const dispatch: AppDispatch = useDispatch();
+	const allEvents = useSelector((state: State) => state.allEvents);
 
-    useEffect(() => {
-        dispatch(getAllEvent())
-    }, [dispatch])
+	useEffect(() => {
+		dispatch(getAllEvent());
+	}, [dispatch]);
 
-    return(
-        <div>
-            <SearchBar/>
-            <div className={s.home}>
-            {
-                allEvents?.map((result: Event) => {
-                    return (
-                        <Card
-                         name={result.name}
-                         image={result.image}
-                         location={result.location}
-                         place={result.place}
-                         date={result.date}
-                        />
-                    )
-                })
-            }
-            </div>
-        </div>
-    )
-}
+	return (
+		<div>
+			<SearchBar />
+			<div className={s.home}>
+				<Events allEvents={allEvents} />
+			</div>
+		</div>
+	);
+};
 
-export default Home
+export default Home;
