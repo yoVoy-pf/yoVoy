9e1ec76708e487b4894c020fd6f4d6107f6e48db
Pablo Pelardas <pablo.pelardas@gmail.com>
2022-06-07 16:01:19 -0300

    feat(api/authorization): JWT generated on login, expires in 30m

--- api/src/controllers/auth.ts
+++ api/src/controllers/auth.ts
@@ -4,6 +4,7 @@ import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import {users} from './users'
 import { iUser } from "../models/User"
+import { generateAccessToken } from "../utils/auth"
 
 export const registerUser = async (req: Request, res: Response, next: NextFunction) => {
   let user : iUser = {name: req.body.name, password: req.body.password}
@@ -22,7 +23,7 @@ export const loginUser = async (req: Request, res: Response, next: NextFunction)
   if (!user) return next({status: 400, message: `Cannot find user`})
   try{
     if(await bcrypt.compare(req.body.password, user.password)){ // Compare the password sent by body with the one in the DB
-      const accessToken = jwt.sign({name: user.name}, process.env.ACCESS_TOKEN_SECRET as string)
+      const accessToken = generateAccessToken(user)
       res.send({accessToken})
     } else res.send('Not Allowed')
   }catch(error){
