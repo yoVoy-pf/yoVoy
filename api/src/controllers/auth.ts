8a20a58526a7180198b7daf35f61d0f198e5433e
Pablo Pelardas <pablo.pelardas@gmail.com>
2022-06-08 17:26:02 -0300

    feat:(api/authorization): refresh token implemented

--- api/src/controllers/auth.ts
+++ api/src/controllers/auth.ts
@@ -1,9 +1,9 @@
 require('dotenv').config()
 import { Request, Response, NextFunction } from "express"
 import bcrypt from 'bcrypt'
-import { createUserInDb, getUserFromDbByName } from "../utils/users"
+import { createUserInDb, getUserFromDbByField } from "../utils/users"
 import { iUser } from "../models/User"
-import { generateAccessToken } from "../utils/auth"
+import { generateAccessToken, updateRefreshToken, verifyRefreshToken } from "../utils/auth"
 
 export const registerUser = async (req: Request, res: Response, next: NextFunction) => {
   let user : iUser = {name: req.body.name, password: req.body.password}
@@ -18,14 +18,31 @@ export const registerUser = async (req: Request, res: Response, next: NextFuncti
 }
 
 export const loginUser = async (req: Request, res: Response, next: NextFunction) => {
-  const user = await getUserFromDbByName(req.body.name) // Find in DB
+  const user = await getUserFromDbByField('name',req.body.name) // Find in DB
   if (!user) return next({status: 400, message: `Cannot find user`})
   try{
     if(await bcrypt.compare(req.body.password, user.password)){ // Compare the password sent by body with the one in the DB
-      const accessToken = generateAccessToken(user)
+      const accessToken =  generateAccessToken(user)
+      const refreshToken = await updateRefreshToken(user)
+      res.cookie('jwt', refreshToken, {httpOnly: true, maxAge: 24 * 60 * 60 * 1000}) // 1 day
       res.send({accessToken})
-    } else res.send('Not Allowed')
+    } else next({status:403 , message:'Not Allowed'})
   }catch(error){
     next(error)
   }
+}
+
+export const handleRefreshToken = async (req: Request, res: Response, next: NextFunction) => {
+  const cookies = req.cookies;
+  if (!Object.keys(cookies).length || !Object.keys(cookies.jwt).length) return next({status: 401, message: `Invalid refresh token`})
+  const refreshToken = cookies.jwt;
+  try{
+    const user = await getUserFromDbByField('refreshToken',refreshToken);
+    if (!user) return next({status:403 , message:'Not Allowed'})
+    let newToken = verifyRefreshToken(user);
+    if (typeof newToken === 'string') res.send({newToken})
+    else throw newToken    
+  }catch(error){return next(error)}
+
+  
 }
--- api/src/routes/auth.ts
+++ api/src/routes/auth.ts
@@ -1,9 +1,10 @@
 import {Router} from 'express'
 export const router = Router();
-import {registerUser, loginUser} from '../controllers/auth'
+import {registerUser, loginUser, handleRefreshToken} from '../controllers/auth'
 
 router.post('/user/register', registerUser)
 router.post('/user/login', loginUser)
+router.get('/user/refresh-token', handleRefreshToken)
 
 
 router.post('/organization/register', () => {})
