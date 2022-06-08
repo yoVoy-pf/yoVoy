bc5da908732c06fcc96253ccd16fc75cfcd66ecb
Pablo Pelardas <pablo.pelardas@gmail.com>
2022-06-08 15:25:48 -0300

    feat(api/authorization): new function to update user refresh token

--- api/src/utils/auth.ts
+++ api/src/utils/auth.ts
@@ -1,7 +1,17 @@
 require('dotenv').config()
 import  jwt  from "jsonwebtoken";
-import { iUser } from "../models/User";
+import { iUser, User } from "../models/User";
 
 export function generateAccessToken(user: iUser){
   return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET as string, {expiresIn: '30m'})
+}
+
+export async function updateRefreshToken(user: iUser){
+  const token = jwt.sign(
+        user,
+        process.env.REFRESH_TOKEN_SECRET as string,
+        {expiresIn: '1d'}
+      )
+  await User.update({refreshToken: token}, {where: {name: user.name}})
+  return token;
 }
