422107aad4efe6ba401e447f03bcf6e763bc49d5
Diego Avila <57416193+Diego-Avila-Acosta@users.noreply.github.com>
2022-06-08 15:30:48 -0300

    fix(api/category controller): added try catch

--- api/src/controllers/category.ts
+++ api/src/controllers/category.ts
@@ -1,9 +1,14 @@
-import { Response, Request } from "express";
+import { Response, Request, NextFunction } from "express";
 import { createCategory } from "../utils/category"
 
-export const postCategory = async (req: Request, res: Response) => {
-    const {name} = req.body
-    const category = await createCategory(name)
+export const postCategory = async (req: Request, res: Response, next: NextFunction) => {
+    try{
+        const {name} = req.body
+        const category = await createCategory(name)
 
-    res.status(200).json(category)
+        res.status(200).json(category)
+    }catch(error){
+        next(error)
+    }
+
 } 
