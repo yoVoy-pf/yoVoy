d339dce410a3089a0b47a68665d1d31340cf426b
Pablo Pelardas <pablo.pelardas@gmail.com>
2022-06-09 15:26:35 -0300

    feat(api/roles): update and genereate token now manage the data field by field

--- api/src/utils/auth.ts
+++ api/src/utils/auth.ts
@@ -2,27 +2,35 @@ require('dotenv').config()
 import  jwt  from "jsonwebtoken";
 import { User } from "../models/User";
 import { iUser } from "../types/user";
+import { DecodedUserInfo } from "../types/utilsTypes";
 
 export function generateAccessToken(user: iUser){
-  let currentUser = {...user};
-  delete currentUser.refreshToken;
-  return jwt.sign(currentUser, process.env.ACCESS_TOKEN_SECRET as string, {expiresIn: '30m'})
+  return jwt.sign({
+    'UserInfo':{
+      'name': user.name,
+      'email': user.email,
+      'rolesId':user.rolesId
+    }
+  }, process.env.ACCESS_TOKEN_SECRET as string, {expiresIn: '30m'})
 }
 
 export async function updateRefreshToken(user: iUser, errase: boolean = false){
   let token;
-  let currentUser = {...user};
-  delete currentUser.refreshToken;
   errase 
   ? token = '' 
   : token = jwt.sign(
-        currentUser,
+        {'UserInfo': {
+          'name': user.name,
+          'email': user.email
+        }},
         process.env.REFRESH_TOKEN_SECRET as string,
         {expiresIn: '1d'}
       )
   await User.update({refreshToken: token}, {where: {name: user.name}})
   return token;
 }
+
+
 
 export function verifyRefreshToken(user:iUser){
   const token = user.refreshToken as string;
@@ -31,8 +39,8 @@ export function verifyRefreshToken(user:iUser){
     token,
     process.env.REFRESH_TOKEN_SECRET as string,
     (err, decoded) => {
-    let decodedUser : iUser = decoded as iUser;
-    if (err || user.name !== decodedUser.name)  newToken =({status:403, message:`You don't have access. Token no longer valid`})
+    let decodedUserInfo = decoded as DecodedUserInfo
+    if (err ||user.name !== decodedUserInfo.UserInfo.name)  newToken =({status:403, message:`You don't have access. Token no longer valid`})
     else newToken = generateAccessToken(user)
   }
   )
