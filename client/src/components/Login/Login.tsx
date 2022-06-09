419c82a63c918b4915ec96c5543bdf06dddd312e
Federico A. Gomez <fmtgomez@gmail.com>
2022-06-09 16:18:30 -0300

    feat(client/Login):render navbar, add button login in home

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import NavBar from "../NavBar/NavBar";
 
 const Login =()=>{
     return(
         <React.Fragment>
-
+            <NavBar/>
         </React.Fragment>
     )
 };
