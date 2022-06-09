1ab46b7ac7de468f031602b4a24e185cc3509aa8
Gonzalo Madona <gonzalomadona42@gmail.com>
2022-06-09 13:24:31 -0300

    (client/form): add form register and login

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -1,9 +1,38 @@
 import React from "react";
 
-const Login =()=>{
+const Login = () => {
+
     return(
         <React.Fragment>
+            <form>
+                <h1>Please login</h1>
 
+                <label>Name</label> <br />
+                <input 
+                type="text" 
+                placeholder="Name" 
+                name='name'
+                required
+                /> <br /> <br />
+
+                <label>Email</label>
+                <input 
+                type="email" 
+                placeholder="Email@Email.com" 
+                name='email'
+                required
+                /> <br /> <br />        
+
+                <label>Pasword</label> <br />
+                <input 
+                type="password"
+                placeholder="Password"
+                name='password'
+                required
+                /> <br /> <br />
+
+                <button type="submit">Iniciar sesion</button> <br /> 
+             </form>
         </React.Fragment>
     )
 };
--- client/src/components/Signup/Signup.tsx
+++ client/src/components/Signup/Signup.tsx
@@ -1,9 +1,38 @@
 import React from "react";
 
-const Signup = ()=>{
+const Signup = () => {
+
     return(
         <React.Fragment>
+            <form>
+                <h1>Please register</h1>
 
+                <label>Name</label> <br />
+                <input 
+                type="text" 
+                placeholder="Name" 
+                name='name'
+                required
+                /> <br /> <br />
+
+                <label>Email</label>
+                <input 
+                type="email" 
+                placeholder="Email@Email.com" 
+                name='email'
+                required
+                /> <br /> <br />
+
+                <label>Pasword</label> <br />
+                <input 
+                type="password"
+                placeholder="Password"
+                name='password'
+                required
+                /> <br /> <br />
+
+                <button type="submit">Registrarse</button> <br /> 
+            </form>
         </React.Fragment>
     )
 };
