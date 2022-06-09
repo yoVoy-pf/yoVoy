d12f13f89208cd605942e5f45b9dec163560afb1
Pablo Pelardas <pablo.pelardas@gmail.com>
2022-06-08 23:59:51 -0300

    feat(api/roles): add "User" role to a new user on registration

--- api/src/utils/users.ts
+++ api/src/utils/users.ts
@@ -1,19 +1,30 @@
 import { sequelize } from '../db'
 import { iUser } from '../models/User';
-const { User } = sequelize.models
+import { ROLES_LIST } from '../authorization/roles';
+const { User, Role, UserRole } = sequelize.models
 
 
 // create new user in the database
 export async function createUserInDb(user: iUser) {
   const {name, password} = user
   try{
-    await User.create({name, password})
+    const newUser = await User.create({name, password})
+    let id : number = newUser?.getDataValue('id')
+    UserRole.create({userId: id, roleId: ROLES_LIST.User})
   }catch(error) {throw error}
 }
 
 //return every user in the database
 export async function getUsersFromDb() {
-    const users = await User.findAll()
+    const users = await User.findAll({
+      include: {
+        model: Role,
+        attributes:['name'],
+        through:{
+          attributes:[]
+        }
+      }
+    })
     return users;
 }
 
@@ -29,4 +40,8 @@ export async function getUserFromDbByField(field: string, value: string) {
     let email : string= user?.getDataValue('email');
     let refreshToken : string = user?.getDataValue('refreshToken');
      return {name: username, password, email, refreshToken};
+}
+
+export async function giveRoleToUser(user: iUser, role: number){
+
 }
