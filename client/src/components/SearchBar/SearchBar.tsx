a87e7c4569dc2a83dff9f8b8e51cff80c973f889
Federico A. Gomez <fmtgomez@gmail.com>
2022-06-09 16:16:45 -0300

    feat(client/Searchbar):create file for css searchbar

--- client/src/components/SearchBar/SearchBar.tsx
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -2,30 +2,33 @@ import React, { useState } from "react";
 import { AppDispatch } from "../../redux/store/store";
 import { useDispatch } from "react-redux";
 import { getSearchEvent } from "../../redux/actions/actions-Create";
+import "./SearchBar.css"
 
 type FormElement = React.FormEvent<HTMLFormElement>;
 type Input = React.ChangeEvent<HTMLInputElement>
 
-const SearchBar = ()=>{
-    const dispatch : AppDispatch = useDispatch();
+const SearchBar = () => {
+    const dispatch: AppDispatch = useDispatch();
     const [input, setInput] = useState("");
 
-    function onSubmit(e:FormElement){
+    function onSubmit(e: FormElement) {
         e.preventDefault();
-        input?dispatch(getSearchEvent(input)): alert("oops! empty field");
+        input ? dispatch(getSearchEvent(input)) : alert("oops! empty field");
         setInput("");
     };
 
-    function onInputChange(e:Input){
+    function onInputChange(e: Input) {
         setInput(e.target.value)
     }
 
-    return(
+    return (
         <React.Fragment>
-            <form onSubmit = {onSubmit}>
-                <input type="text" onChange={onInputChange} value={input} placeholder="Buscar..." />
-                <input type="submit" value ="🔍" />
-            </form>
+            
+                <form onSubmit={onSubmit}>
+                    <input className="bg"  type="text" onChange={onInputChange} value={input} placeholder="Buscar..." />
+                    <input className="input2" type="submit" value="🔍" />
+                </form>
+          
 
         </React.Fragment>
     )
