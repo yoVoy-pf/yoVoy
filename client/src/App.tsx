179c61a35ec90c437ab4608a3d06b2125ee5da71
Pablo Pelardas <pablo.pelardas@gmail.com>
2022-06-10 18:26:07 -0300

    feat(client/authentication): add auth check on reload and getUserAuth query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,35 @@
-// import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Event from './components/Event/Event';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
-// import NavBar from './components/NavBar/NavBar';
 import Signup from './components/Signup/Signup';
 import Layout from './components/Layout/Layout';
 import RequireAuth from './components/RequireAuth.ts/RequireAuth';
 import { Welcome } from './components/Welcome/Welcome';
 import UsersList from './components/UsersList/UsersList';
+import { useGetUserAuthQuery } from './authentication/authApiSlice'
+
 
 function App(): JSX.Element {
-	return (
-			<Routes>
-        <Route path='/' element={<Layout/>}>
-          {/* public routes */}
-          <Route index element={<Home />} />
-          <Route path="events/:id" element={<Event />} />
-          <Route path='login' element= {<Login/>}/>
-          <Route path='signup' element={<Signup/>}/>
-          {/* protected routes */}
-          <Route element={<RequireAuth/>}>
-            <Route path='welcome' element={<Welcome />}/>
-            <Route path='userslist' element={<UsersList />}/>
-          </Route>
+  useGetUserAuthQuery();
+
+  return (
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        {/* public routes */}
+        <Route index element={<Home />} />
+        <Route path="events/:id" element={<Event />} />
+        <Route path='login' element={<Login />} />
+        <Route path='signup' element={<Signup />} />
+        {/* protected routes */}
+        <Route element={<RequireAuth />}>
+          <Route path='welcome' element={<Welcome />} />
+          <Route path='userslist' element={<UsersList />} />
         </Route>
-			</Routes>
-	);
+      </Route>
+    </Routes>
+  )
 }
+
 
 export default App;
