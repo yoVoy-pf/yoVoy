9fb2baae4e6021d99deeb36e4268b1b5295b8179
Mariano Dunand <marianoo.14.md@gmail.com>
2022-06-09 21:55:06 -0300

    fix(client/event): v1.1

--- client/src/components/Event/Event.tsx
+++ client/src/components/Event/Event.tsx
@@ -4,11 +4,13 @@ import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { clearEventId, getEventId } from '../../redux/actions/actions-Create';
 import { AppDispatch, State } from '../../redux/store/store';
-import { Location } from '../../types';
+import { Dates, Location } from '../../types';
 
 const Event = () => {
 	const dispatch: AppDispatch = useDispatch();
-	const eventDetail: any = useSelector((state: State) => state.global.eventDetail);
+	const eventDetail: any = useSelector(
+		(state: State) => state.global.eventDetail,
+	);
 	const { id }: any = useParams<{ id: string }>();
 
 	useEffect(() => {
@@ -21,20 +23,22 @@ const Event = () => {
 	return (
 		<React.Fragment>
 			<img src={eventDetail.background_image} alt={eventDetail.name} />
-			<h1>{eventDetail.name}</h1>
+			<h1>Evento: {eventDetail.name}</h1>
 
-			<br />
-			<h4>Precios</h4>
 			{eventDetail &&
 				eventDetail.locations?.map((location: Location) => {
 					return (
-						<div key={location.id}>
-							<p>-{location.name}</p>
-							<p>-{location.address}</p>
-						</div>
+						<React.Fragment key={location.id}>
+							{/*En los 2 siguientes h4, reemplazar Lugar: y Direc: por iconos =) */}
+							<h4>Lugar: {location.name}</h4>
+							<h4>Direc: {location.address}</h4>
+						</React.Fragment>
 					);
 				})}
-
+			<button>Ver todas las fechas y precios</button>
+			<hr />
+			<button>COMPRAR</button>
+			<hr />
 			<small>{eventDetail.description}</small>
 		</React.Fragment>
 	);
