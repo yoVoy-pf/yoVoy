7b610b324886852abaa296810ca99ee4b7519c82
unknown <lucasmontero.wed.odin@gmail.com>
2022-06-10 00:16:49 -0300

    feat(client/components filterEvents)add option list cities

--- client/src/components/FilterEvents/FilterEvents.tsx
+++ client/src/components/FilterEvents/FilterEvents.tsx
@@ -7,12 +7,15 @@ import {
 	getCities,
 } from '../../redux/actions/actions-Create';
 import { AppDispatch, State } from '../../redux/store/store';
-import { Category } from '../../types';
+import { Category, City } from '../../types';
 
 const FilterEvent = () => {
 	const dispatch: AppDispatch = useDispatch();
 	const categories: Array<Category> = useSelector(
 		(state: State) => state.global.categories,
+	);
+	const cities: Array<City> = useSelector(
+		(state: State) => state.global.cities,
 	);
 
 	useEffect(() => {
@@ -29,6 +32,14 @@ const FilterEvent = () => {
 				{categories.map((c) => (
 					<option key={c.name} value={c.id}>
 						{c.name}
+					</option>
+				))}
+			</select>
+
+			<select >
+				{cities.map((city) => (
+					<option key={city.name} value={city.id}>
+						{city.name}
 					</option>
 				))}
 			</select>
