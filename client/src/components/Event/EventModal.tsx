16153ea4026b726bbfbd28a140506ff73655f364
Mariano Dunand <marianoo.14.md@gmail.com>
2022-06-10 03:30:03 -0300

    refactor(client/events): v 2.0, ModalDates

---
+++ client/src/components/Event/EventModal.tsx
@@ -0,0 +1,21 @@
+import './EventModal.css';
+
+const EventModal = ({ children, isOpen, closeModal }: any | boolean) => {
+	const handleModalContainerClick = (event: React.MouseEvent<HTMLElement>) =>
+		event.stopPropagation();
+	return (
+		<article
+			className={`modal ${isOpen && 'is-open'}`}
+			onClick={() => closeModal}
+		>
+			<div className="modal-container" onClick={handleModalContainerClick}>
+				<button className="modal-close" onClick={() => closeModal}>
+					X
+				</button>
+				{children}l
+			</div>
+		</article>
+	);
+};
+
+export default EventModal;
