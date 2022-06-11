08c027f0d1054115c0036e742959696c1e11ae16
Avila Diego <aviladiego2009@gmail.com>
2022-06-11 14:05:21 -0300

    feat(api/POST favorites): added routes, controllers and utils

--- api/src/controllers/user.ts
+++ api/src/controllers/user.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express"
-import { getAllFavorites} from "../utils/user"
+import { createFavorite, getAllFavorites} from "../utils/user"
 
 export const getFavorites = async (req: Request,res: Response,next:NextFunction) =>{
     try{
@@ -8,6 +8,19 @@ export const getFavorites = async (req: Request,res: Response,next:NextFunction)
         const favorites = await getAllFavorites(user.id)
         if(!favorites.length) next({status: 404, message: `Event/s not found`})
         else res.status(200).json(favorites)
+    }catch(error){
+        next(error)
+    }
+}
+
+export const postFavorite = async (req: Request,res: Response,next:NextFunction) =>{
+    try{
+        const {user} = req.body
+        const eventId = req.params.eventId as string
+
+        const favorite = await createFavorite(user.id, eventId)
+        
+         res.status(200).json(favorite)
     }catch(error){
         next(error)
     }
