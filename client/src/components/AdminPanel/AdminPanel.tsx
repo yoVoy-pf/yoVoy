72f036d1fb5e39684d43814c2505b7cb9d52471f
Gonzalo Madona <gonzalomadona42@gmail.com>
2022-06-12 17:12:05 -0300

    feat(client/style-panel-admin): add style component admin panel

--- client/src/components/AdminPanel/AdminPanel.tsx
+++ client/src/components/AdminPanel/AdminPanel.tsx
@@ -1,19 +1,33 @@
 import { Divider, Stack } from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styleAdminPanel from './admin-panel.module.css'
 
 const AdminPanel = () => {
   return (
-    <div><h1>Panel de Administrador</h1>
+    <div>
+      <div className={styleAdminPanel.title_admin}>
+        <div className={styleAdminPanel.title_style}>
+          <h1>Panel de Administrador</h1>
+        </div>
+      </div>
+      <div className={styleAdminPanel.buttons_admin}>
       <Stack
         direction="row"
         divider={<Divider orientation="vertical" flexItem />}
         spacing={2}
       >
-        <button><Link to='/userslist'>Go to the Users List</Link></button>
-        <button><Link to='/create-category'>Crear Category</Link></button>
-        <button>button 3</button>
+    
+            <button className={styleAdminPanel.buttons_style}>
+              <Link className={styleAdminPanel.links_style} to='/userslist'>Ir a la lista de usuarios</Link>
+              </button>
+            <button className={styleAdminPanel.buttons_style}>
+              <Link className={styleAdminPanel.links_style} to='/create-category'>Crear Categoria</Link>
+              </button>
+            {/* <button className={styleAdminPanel.buttons_style}>button 3</button> */}
+
       </Stack>
+        </div>
     </div>
   )
 }
