e6ed9ba110dbddd95024d4a59b1eb7673af01d56
Avila Diego <aviladiego2009@gmail.com>
2022-06-11 22:40:28 -0300

    feat(api/models): added new attributes

--- api/src/models/User.ts
+++ api/src/models/User.ts
@@ -17,6 +17,12 @@ export class User extends Model<iUser>{
   email!: string;
 
   @Column
+  surname!: string;
+
+  @Column
+  DNI!: number;
+
+  @Column
   password!: string
 
   @Column(DataType.STRING(500))
