0d8a6dea04ec9f2ab22237945b1df9f20c1bd874
Pablo Pelardas <pablo.pelardas@gmail.com>
2022-06-12 15:11:54 -0300

    feat(client/update-event):  new update event

--- client/src/components/UpdateEvent/UpdateEvent.tsx
+++ client/src/components/UpdateEvent/UpdateEvent.tsx
@@ -1,137 +1,221 @@
-import React, { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import {
+  getCategories,
+  getLocations,
+} from '../../redux/actions/actions-Create';
+import { AppDispatch, State } from '../../redux/store/store';
+import { Category, Location } from '../../types';
+import DatesModal from '../CreateEvent/CreateEventModal/DatesModal'
+import { useCreateEvent } from '../CreateEvent/useCreateEvent';
+import { useDatesModal } from '../CreateEvent/CreateEventModal/useDatesModal';
 import { useGetEventQuery, useUpdateEventMutation } from "../../slices/app/eventsApiSlice"
-import { useParams } from "react-router-dom";
-import { putEvent } from "../../types";
+import { useNavigate, useParams } from "react-router-dom";
 
-type Input = React.ChangeEvent<HTMLInputElement>;
-type InputText = React.ChangeEvent<HTMLTextAreaElement>;
+interface CreateEventState {
+  event: Array<{}>;
+}
 
 const UpdateEvent = () => {
-    const navigate = useNavigate()
-    const {eventId} = useParams()
-    const {data: eventInfo} = useGetEventQuery({id: '1'});
-    console.log(eventInfo)
-    const [updateEvent] = useUpdateEventMutation()
+  const [isOpenModal, openModal, closeModal] = useDatesModal(false);
+  const [updateEvent] = useUpdateEventMutation();
+  const dispatch: AppDispatch = useDispatch();
+  const { eventId } = useParams();
+  const { data: eventInfo } = useGetEventQuery({ id: eventId as string | '1' })
 
-    useEffect(() => {
-      let mappedCategoriesIds = eventInfo?.categories?.map((c: any) => c.id)
-      let mappedLocations = eventInfo?.locations?.map((loc: any) => ({
-        id: loc.id,
-        dates: loc.dates
-      }),[eventInfo])
+  const navigate = useNavigate()
 
-      console.log({mappedLocations})
-      setEvent({
-        name: eventInfo?.name || '',
-        description: eventInfo?.description || '',
-        background_image: eventInfo?.background_image || '',
-        categoriesIds: mappedCategoriesIds || [],
-        locations: mappedLocations || [],
-      })
-    },[eventInfo])
+  const locations: Array<Location> = useSelector(
+    (state: State) => state.global.locations,
+  );
+  const categories: Array<Category> = useSelector(
+    (state: State) => state.global.categories,
+  );
 
-    const [event, setEvent] = useState<putEvent>({
+
+  useEffect(() => {
+    dispatch(getLocations());
+    dispatch(getCategories());
+  }, [dispatch]);
+
+  useEffect(() => {
+    let mappedCategoriesIds = eventInfo?.categories?.map((c: any) => c.id)
+    // let mappedLocations = eventInfo?.locations?.map((loc: any) => ({
+    //   id: loc.id,
+    //   dates: loc.dates
+    // }))
+
+    // console.log({ mappedLocations })
+    setInput({
+      name: eventInfo?.name || '',
+      description: eventInfo?.description || '',
+      background_image: eventInfo?.background_image || '',
+      locations: eventInfo?.locations[0].id || '',
+      categories: mappedCategoriesIds || [],
+      dates: eventInfo?.locations[0].dates || []
+    })
+  }, [eventInfo])
+
+  const [event, setEvent] = useState<CreateEventState['event']>([]);
+
+  const [
+    input,
+    handleInputChange,
+    setInput,
+    handleCategoryChange,
+    handleLocationChange,
+    inputDate,
+    setInputDate,
+    handleInputDateChange,
+  ] = useCreateEvent({
+    name: '',
+    description: '',
+    background_image: '',
+    locations: '',
+    categories: [],
+    dates: [],
+  });
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // dispatch(postCreateEvent(input));
+    try {
+      eventId && await updateEvent({id: eventId ,updatedEvent: input })
+      setEvent([...event, input]);
+      setInput({
         name: '',
         description: '',
         background_image: '',
-        categoriesIds: [],
-        locations: [],
-    });
-    
-    const onInputChange = (e: Input | InputText) => {
-        e.preventDefault();
-      if (e.target.name === 'price' || e.target.name === 'date'){
-            console.log({...event})
-          let response: any = JSON.parse(JSON.stringify(event.locations));
-            let newDate = {...response[0].dates[0]}
-            newDate[e.target.name] = e.target.value
-            console.log('response:', response[0].dates[0])
-            console.log('newDate:', newDate)
-            Object.assign(response[0].dates[0], newDate)
-            // response[0].dates[0] = newDate
-            console.log(response)
-            setEvent({
-                ...event,
-                locations: response
-            })
-        } else {
-            setEvent({
-                ...event,
-                [e.target.name]: e.target.value,
-              });
-        }
-       
-    }
+        locations: '',
+        categories: [],
+        dates: [],
+      });
+      navigate('/')
+    } catch (error) { console.log(error) }
+  };
 
-    const onSubmit = async (e: any) => {
-        e.preventDefault();
-        console.log({eventId});
-        const updatedData = {
-          name: event.name,
-          description: event.description,
-          background_image: event.background_image,
-          categoriesIds: event.categoriesIds,
-          locations: event.locations
-        }
-        eventId && await updateEvent({id:eventId, updatedEvent:updatedData })
-        setEvent({
-            name: '',
-            description: '',
-            background_image: '',
-            categoriesIds:[],
-            locations: [],
-        })
-        navigate('/')
-    }
-    return(
-        <div>
-            <form onSubmit={onSubmit}>
-                <label>Nombre del Evento</label> <br />
-                <input 
-                    type="text" 
-                    value={event.name}
-                    name='name'
-                    onChange={onInputChange}
-                /> <br />
-                <label>Descripción</label> <br />
-                <textarea 
-                    value={event.description}
-                    name='description'
-                    onChange={onInputChange}
-                /> <br />
-                <label>Imagen</label> <br />
-                <input 
-                    type="text" 
-                    value={event.background_image}
-                    name='background_image'
-                    onChange={onInputChange}
-                /> <br />
-                <label>Categoria</label> <br />
+  const addDate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setInput({ ...input, dates: [...input.dates, inputDate] });
+    setInputDate({ price: 0, date: '' });
+    closeModal();
+  };
+  console.log(input)
+
+  const getLocationFormatted = (locId: number) => {
+    const loc = locations?.find((loc) => loc?.id === input.locations)
+    return `-${loc?.id}, ${loc?.address}, ${loc?.name}, ${loc?.city['name']}.`
+  }
+
+  return (
+    <React.Fragment>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label htmlFor="name">Nombre del evento:</label>
+          <input
+            name="name"
+            type="text"
+            id="name"
+            placeholder="Nombre del evento"
+            onChange={handleInputChange}
+            value={input.name}
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="description">Descripcion:</label>
+          <textarea
+            name="description"
+            id="description"
+            placeholder="Descripcion..."
+            onChange={handleInputChange}
+            value={input.description}
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="background_image">Imagen:</label>
+          <input
+            name="background_image"
+            type="text"
+            id="background_image"
+            placeholder="Imagen..."
+            onChange={handleInputChange}
+            value={input.background_image}
+          />
+        </fieldset>
+        <fieldset>
+          <select
+            name="locations"
+            id="locations"
+            onChange={handleLocationChange}
+            defaultValue={"default"}
+          >
+            <option value="default" disabled>{input.locations ? getLocationFormatted(input.locations) : `Seleccione la ciudad...`}</option>
+            {locations?.map((location: Location) => {
+              return (
+                <option key={location.id} value={location.id} >
+                  {`-${location.id}, ${location.address}, ${location.name}, ${location.city['name']}.`}
+                </option>
+              );
+            })}
+          </select>
+        </fieldset>
+
+        <fieldset>
+          <label htmlFor="categories">Seleccione las categorias...</label>
+          {categories?.map((category: Category) => {
+            return (
+              <React.Fragment key={category.id}>
                 <input
-                    type="text" 
-                    value={event.categoriesIds[0]}
-                    name='categoriesIds'
-                    onChange={onInputChange}
-                /> <br />
-                <label>Precio</label> <br />
-                <input 
-                    type="text" 
-                    value={event?.locations[0]?.dates[0]?.price}
-                    name='price'
-                    onChange={onInputChange}
-                /> <br />
-                <label>Fecha</label> <br />
-                <input 
-                    type="date" 
-                    value={event?.locations[0]?.dates[0]?.date}
-                    name='date'
-                    onChange={onInputChange}
-                /> <br />
-                <button type="submit">¡Actualizar Evento!</button>
-            </form>
-        </div>
-    )
-}
+                  value={category.id}
+                  type="checkbox"
+                  checked={input.categories.includes(category.id)}
+                  onChange={handleCategoryChange}
+                />
+                <span>{`-${category.id}, ${category.name}`}.</span>
+              </React.Fragment>
+            );
+          })}
+        </fieldset>
 
-export default UpdateEvent
+        <fieldset>
+          <button onClick={openModal} type="button">
+            Agregar fechas
+          </button>
+          <DatesModal isOpen={isOpenModal} closeModal={closeModal}>
+            <h3>Agregar fechas.</h3>
+
+            <input
+              name="price"
+              type="number"
+              id="price"
+              onChange={handleInputDateChange}
+              value={inputDate.price}
+            />
+            <input
+              name="date"
+              type="date"
+              id="date"
+              onChange={handleInputDateChange}
+              value={inputDate.date}
+            />
+
+            <button onClick={(e: any) => addDate(e)}>+</button>
+          </DatesModal>
+
+          {input.dates?.map((date: any) => {
+            return (
+              <React.Fragment key={date.date}>
+                <h5>$: {date.price}</h5>
+                <h5>Fecha: {date.date}</h5>
+              </React.Fragment>
+            );
+          })}
+        </fieldset>
+        <button type="submit">Create</button>
+      </form>
+    </React.Fragment>
+  );
+};
+
+export default UpdateEvent;
