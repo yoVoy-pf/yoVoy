7d9b8756a684d55217f590caa8e345f6451b8d43
Gonzalo Madona <gonzalomadona42@gmail.com>
2022-06-12 14:44:21 -0300

    feat(client/style-create-event): add style component create event

--- client/src/components/CreateEvent/CreateEvent.tsx
+++ client/src/components/CreateEvent/CreateEvent.tsx
@@ -12,6 +12,7 @@ import DatesModal from './CreateEventModal/DatesModal';
 import { useCreateEvent } from './useCreateEvent';
 import { useDatesModal } from './CreateEventModal/useDatesModal';
 import {useCreateEventMutation} from '../../slices/app/eventsApiSlice'
+import styleCreateEvent from './create-event.module.css'
 
 interface CreateEventState {
 	event: Array<{}>;
@@ -81,78 +82,90 @@ const CreateEvent = () => {
 	return (
 		<React.Fragment>
 			<form onSubmit={handleSubmit}>
-				<fieldset>
-					<label htmlFor="name">Nombre del evento:</label>
+			<div className={styleCreateEvent.form_order}>
+				<fieldset className={styleCreateEvent.fieldset_form}>
+					{/* <label htmlFor="name">Nombre del evento:</label> */}
+					<legend className={styleCreateEvent.legend_form}>Nombre del evento:</legend>
 					<input
 						name="name"
 						type="text"
 						id="name"
 						placeholder="Nombre del evento"
+						className={styleCreateEvent.input_create}
 						onChange={handleInputChange}
 						value={input.name}
 					/>
-				</fieldset>
-				<fieldset>
-					<label htmlFor="description">Descripcion:</label>
+				</fieldset> <br />
+				<fieldset className={styleCreateEvent.fieldset_form}>
+					{/* <label htmlFor="description">Descripcion:</label> */}
+					<legend className={styleCreateEvent.legend_form}>Descripcion:</legend>
 					<input
 						name="description"
 						type="text"
 						id="description"
 						placeholder="Descripcion..."
+						className={styleCreateEvent.input_create}
 						onChange={handleInputChange}
 						value={input.description}
 					/>
-				</fieldset>
-				<fieldset>
-					<label htmlFor="background_image">Imagen:</label>
+				</fieldset> <br />
+				<fieldset className={styleCreateEvent.fieldset_form}>
+					{/* <label htmlFor="background_image">Imagen:</label> */}
+					<legend className={styleCreateEvent.legend_form}>Imagen:</legend>
 					<input
 						name="background_image"
 						type="text"
 						id="background_image"
 						placeholder="Imagen..."
+						className={styleCreateEvent.input_create}
 						onChange={handleInputChange}
 						value={input.background_image}
 					/>
-				</fieldset>
-				<fieldset>
+				</fieldset> <br />
+				<fieldset className={styleCreateEvent.fieldset_form}>
+					<legend className={styleCreateEvent.legend_form}>Seleccione la ciudad:</legend>
 					<select
 						name="locations"
 						id="locations"
+						className={styleCreateEvent.form_cities}
 						onChange={handleLocationChange}
 					>
 						<option value="default">Seleccione la ciudad...</option>
 						{locations?.map((location: Location) => {
 							return (
-								<option key={location.id} value={location.id}>
+								<option key={location.id} value={location.id} className={styleCreateEvent.form_citie}>
 									{`-${location.id}, ${location.address}, ${location.name}, ${location.city['name']}.`}
 								</option>
 							);
 						})}
 					</select>
-				</fieldset>
+				</fieldset> <br />
 
-				<fieldset>
-					<label htmlFor="categories">Seleccione las categorias...</label>
+				<fieldset className={styleCreateEvent.fieldset_form}>
+					{/* <label htmlFor="categories">Seleccione las categorias...</label> <br /> */}
+					<legend className={styleCreateEvent.legend_form}>Seleccione las categorias:</legend>
 					{categories?.map((category: Category) => {
 						return (
 							<React.Fragment key={category.id}>
+								<br />
 								<input
 									value={category.id}
 									type="checkbox"
 									onChange={handleCategoryChange}
 								/>
-								<span>{`-${category.id}, ${category.name}`}.</span>
+								<span>{` ${category.id}  ${category.name}`}.</span>
 							</React.Fragment>
 						);
 					})}
-				</fieldset>
+				</fieldset> <br />
 
-				<fieldset>
-					<button onClick={openModal} type="button">
+				<fieldset className={styleCreateEvent.fieldset_form}>
+					<legend className={styleCreateEvent.legend_form}>Seleccione Fecha:</legend>
+					<button onClick={openModal} type="button"  className={styleCreateEvent.text_from} >
 						Agregar fechas
 					</button>
-					<DatesModal isOpen={isOpenModal} closeModal={closeModal}>
-						<h3>Agregar fechas.</h3>
+					<DatesModal isOpen={isOpenModal} closeModal={closeModal} className={styleCreateEvent.form_dates_modal}>
+						<h3 className={styleCreateEvent.form_text_event}>Agregar fechas.</h3>
 
 						<input
 							name="price"
@@ -175,13 +188,14 @@ const CreateEvent = () => {
 					{input.dates?.map((date: any) => {
 						return (
 							<React.Fragment key={date.date}>
-								<h5>$: {date.price}</h5>
-								<h5>Fecha: {date.date}</h5>
+								<h5 className={styleCreateEvent.form_text_color}>$: {date.price}</h5>
+								<h5 className={styleCreateEvent.form_text_color}>Fecha: {date.date}</h5>
 							</React.Fragment>
 						);
 					})}
 				</fieldset>
-				<button type="submit">Create</button>
+				<button type="submit" className={styleCreateEvent.bottom_form}>Create</button>
+				</div>
 			</form>
 		</React.Fragment>
 	);
