017c4dc9edc6ae9646b6450c4a116348878a62b4
Gonzalo Madona <99615258+FireDash22@users.noreply.github.com>
2022-06-12 01:45:40 -0300

    Revert "Refactor/filter front not found manager"

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -3,25 +3,25 @@ import { NavLink } from "react-router-dom";
 import { Link } from "react-router-dom";
 import image from "../../img/oie_transparent.png"
 import "./NavBar.css"
+import { selectCurrentUser } from "../../slices/authentication/authSlice";
+import { useSelector } from "react-redux";
+import Logout from "../Logout/Logout";
 const NavBar = ()=>{
-    const rechargePage = () => {
-        if (window.location.href === "http://localhost:3000/") {
-            window.location.reload()
-        }
-    }
+    const currentUser = useSelector(selectCurrentUser)
     return(
         <nav className="navbar">
-            <Link to="/" className="navbar-log" onClick={()=>rechargePage()}>
+            <Link to="/" className="navbar-log">
                 <img src={image} alt="img" />
             </Link>
             <div className="navbar_bg_mid">
                 <ul className="navbar_bg_ul">
-                    <li className="navbar_bg_li"> <NavLink className="navbar_bg_a" to="/" onClick={()=>rechargePage()}>Home</NavLink> </li>
+                    <li className="navbar_bg_li"> <NavLink className="navbar_bg_a" to="/">Home</NavLink> </li>
                     <li className="navbar_bg_li"> <NavLink className="navbar_bg_a" to="#">Sobre Nosotros</NavLink> </li>
                     <li className="navbar_bg_li"> <NavLink className="navbar_bg_a" to="#">Contacto</NavLink> </li>
                     <li className="navbar_bg_li"> <NavLink className="navbar_bg_a" to="/update-event/1">Put event 1</NavLink> </li>
                 </ul>
             </div>
+            {!currentUser  && 
 
             <div className="prueba_btn">
                 <Link to="/login">
@@ -31,6 +31,12 @@ const NavBar = ()=>{
                     <button className="navBar-btn-register">Registrarse</button>
                 </Link>
             </div>
+            }
+            { currentUser &&
+                <div>
+                    <Logout/>
+                </div>
+            }
         </nav>
     )
 }
