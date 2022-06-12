8df2098211f9aaf0693180d8cbbd0618e8c5ff64
Federico A. Gomez <fmtgomez@gmail.com>
2022-06-12 19:04:24 -0300

    feat(client/event-style):style for event detail

--- client/src/components/Event/EventModal.tsx
+++ client/src/components/Event/EventModal.tsx
@@ -3,7 +3,7 @@ const EventModal = ({ children, isOpen, closeModal }: any | void) => {
 	const handleModalContainerClick = (event: React.MouseEvent<HTMLElement>) =>
 		event.stopPropagation();
 	return (
-		<article className={`modal ${isOpen && 'is-open'}`} onClick={closeModal}>
+		<article  className={`modal ${isOpen && 'is-open'}`} onClick={closeModal}>
 			<div className="modal-container" onClick={handleModalContainerClick}>
 				<button className="modal-close" onClick={closeModal}>
 					X
