50e24a1c14d7a90f46edecee7276d85ba12350a8
Gonzalo Madona <gonzalomadona42@gmail.com>
2022-06-12 16:36:58 -0300

    feat(client/style-welcome): add style component welcome and list user

--- client/src/components/UsersList/UsersList.tsx
+++ client/src/components/UsersList/UsersList.tsx
@@ -1,5 +1,6 @@
 import { useGetUsersQuery } from "../../slices/app/usersApiSlice";
 import { Link } from "react-router-dom";
+import styleUserList from './user-list.module.css'
 
 const UsersList = () => {
   const{
@@ -16,13 +17,23 @@ const UsersList = () => {
   } else if (isSuccess){
     content = (
       <section>
-        <h1>Users List</h1>
-        <ul>
-          {users.map((user: any, i: number) => {
-            return <li key={i}>{user.name}</li>
-          })}
-        </ul>
+        <div className={styleUserList.user_list}>
+        <div className={styleUserList.text_user_list}>
+          <h1>Users List</h1>
+        </div>
+        <div className={styleUserList.text_user_list}>
+          <div className={styleUserList.list_user}>
+            <ul>
+              {users.map((user: any, i: number) => {
+                return <li key={i}>{user.name}</li>
+              })}
+            </ul>
+          </div>
+        </div>
+        <div>
         <Link to='/welcome'>Back to Welcome</Link>
+        </div>
+        </div>
       </section>
     )
   } else if (isError){
