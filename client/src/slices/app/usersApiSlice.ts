a7aa82d3ef65813b49aad43fecb044ca624f14f9
Gonzalo Madona <gonzalomadona42@gmail.com>
2022-06-13 21:29:06 -0300

    feat(client/query): add query delete user

--- client/src/slices/app/usersApiSlice.ts
+++ client/src/slices/app/usersApiSlice.ts
@@ -5,10 +5,19 @@ export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: builder => ({
     getUsers: builder.query<User[],void>({
       query: () => '/api/users',
-    })
+    }),
+    deleteUser: builder.mutation<User,{id: number}>({
+      query(id) {
+        return {
+          url:`api/users/${id}`,
+          method: `Delete`
+        }
+      }
+    }) 
   })
 })
 
 export const{
-  useGetUsersQuery
+  useGetUsersQuery,
+  useDeleteUserMutation,
 } = usersApiSlice
