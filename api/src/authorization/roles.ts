2621d7048cab3f93096d9cb99fe11349cb798b9c
Pablo Pelardas <pablo.pelardas@gmail.com>
2022-06-15 10:55:41 -0300

    deploy(api): preparations for deploy

--- api/src/authorization/roles.ts
+++ api/src/authorization/roles.ts
@@ -16,33 +16,33 @@ const {Role, User, UserRole} = sequelize.models
 
 export async function createRoles(){
   Role.bulkCreate(roles)
-  let password = 'Administrador';
-  let hashedPassword = await bcrypt.hash(password,10)
-  let user = await User.create({
-    name: 'Administrador',
-    password: hashedPassword,
-    email: 'admin@gmail.com'
-  })
-  UserRole.create({userId: user.getDataValue('id'), roleId: ROLES_LIST.Admin })
-  UserRole.create({userId: user.getDataValue('id'), roleId: ROLES_LIST.Organization })
-  UserRole.create({userId: user.getDataValue('id'), roleId: ROLES_LIST.User })
+  // let password = 'Administrador';
+  // let hashedPassword = await bcrypt.hash(password,10)
+  // let user = await User.create({
+  //   name: 'Administrador',
+  //   password: hashedPassword,
+  //   email: 'admin@gmail.com'
+  // })
+  // UserRole.create({userId: user.getDataValue('id'), roleId: ROLES_LIST.Admin })
+  // UserRole.create({userId: user.getDataValue('id'), roleId: ROLES_LIST.Organization })
+  // UserRole.create({userId: user.getDataValue('id'), roleId: ROLES_LIST.User })
 
-  password = 'Organizacion'
-  hashedPassword = await bcrypt.hash(password,10)
-  user = await User.create({
-    name: 'Organizacion',
-    password: hashedPassword,
-    email: 'org@gmail.com',
-  })
-  UserRole.create({userId: user.getDataValue('id'), roleId: ROLES_LIST.Organization })
-  UserRole.create({userId: user.getDataValue('id'), roleId: ROLES_LIST.User })
+  // password = 'Organizacion'
+  // hashedPassword = await bcrypt.hash(password,10)
+  // user = await User.create({
+  //   name: 'Organizacion',
+  //   password: hashedPassword,
+  //   email: 'org@gmail.com',
+  // })
+  // UserRole.create({userId: user.getDataValue('id'), roleId: ROLES_LIST.Organization })
+  // UserRole.create({userId: user.getDataValue('id'), roleId: ROLES_LIST.User })
 
-  password = 'Usuario'
-  hashedPassword = await bcrypt.hash(password,10)
-  user = await User.create({
-    name: 'Usuario',
-    password: hashedPassword,
-    email: 'user@gmail.com',
-  })
-  UserRole.create({userId: user.getDataValue('id'), roleId: ROLES_LIST.User })
+  // password = 'Usuario'
+  // hashedPassword = await bcrypt.hash(password,10)
+  // user = await User.create({
+  //   name: 'Usuario',
+  //   password: hashedPassword,
+  //   email: 'user@gmail.com',
+  // })
+  // UserRole.create({userId: user.getDataValue('id'), roleId: ROLES_LIST.User })
 }
