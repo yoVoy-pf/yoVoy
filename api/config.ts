dcb82538203c06ba5e8805ce33c81cab490f7497
Pablo Pelardas <pablo.pelardas@gmail.com>
2022-06-15 11:16:29 -0300

    nodemon to dependencies

--- api/config.ts
+++ api/config.ts
@@ -12,9 +12,6 @@ const config = {
  port: process.env.PORT || '3001',
  host: process.env.HOST || 'localhost',
  cors: process.env.CORS || 'localhost:3000',
- ACCESS_TOKEN: process.env.ACCESS_TOKEN || "TEST-6618463267359012-061115-bb4818d20c1253649161e38df3c16fc7-612785330",
- CLIENT_ID: process.env.CLIENT_ID,
- CLIENT_SECRET: process.env.CLIENT_SECRET
 };
 
 export default config;
