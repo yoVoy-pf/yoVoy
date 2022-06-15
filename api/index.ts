aa0d8913a4213549fb7a3c775edbb285dcfc2813
Pablo Pelardas <pablo.pelardas@gmail.com>
2022-06-15 15:01:28 -0300

    force true

--- api/index.ts
+++ api/index.ts
@@ -3,17 +3,17 @@
 import app from './src/app';
 import {sequelize} from './src/db'
 import config from "./config"
-// import { MockData } from './MockData/Mock';
+import { MockData } from './MockData/Mock';
 //Mock Data
-// import { createRoles } from './src/authorization/roles';
+import { createRoles } from './src/authorization/roles';
 
 // Syncing all the models at once.
-sequelize.sync({ force: false , logging: false}).then(() => {
+sequelize.sync({ force: true , logging: false}).then(() => {
 	app.listen(config.port, () => {
 		console.log(`%s listening at ${config.port}`); // eslint-disable-line no-console
 
-		//Mock Data
-    // createRoles()
-		// MockData()
+		// Mock Data
+    createRoles()
+		MockData()
 	});
 });
