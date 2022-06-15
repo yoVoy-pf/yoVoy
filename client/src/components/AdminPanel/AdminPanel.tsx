050ed764d6b1b82bab87882e70d8032898d91f27
shek1ng <sheking142@gmail.com>
2022-06-15 16:12:27 -0300

    feat(client/components/AdminPanel): add new button to events config

--- client/src/components/AdminPanel/AdminPanel.tsx
+++ client/src/components/AdminPanel/AdminPanel.tsx
@@ -1,38 +1,47 @@
-import { Divider, Stack } from '@mui/material'
-import React from 'react'
-import { Link } from 'react-router-dom'
-import styleAdminPanel from './admin-panel.module.css'
+import { Divider, Stack } from '@mui/material';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styleAdminPanel from './admin-panel.module.css';
 
 const AdminPanel = () => {
-  return (
-    <div>
-      <div className={styleAdminPanel.title_admin}>
-        <div className={styleAdminPanel.title_style}>
-          <h1>Panel de Administrador</h1>
-        </div>
-      </div>
-      <div className={styleAdminPanel.buttons_admin}>
-      <Stack
-        direction="row"
-        divider={<Divider orientation="vertical" flexItem />}
-        spacing={2}
-      >
-    
-            <button className={styleAdminPanel.buttons_style}>
-              <Link className={styleAdminPanel.links_style} to='/userslist'>Ir a la lista de usuarios</Link>
-              </button>
-            <button className={styleAdminPanel.buttons_style}>
-              <Link className={styleAdminPanel.links_style} to='/create-category'>Crear Categoria</Link>
-              </button>
-            <button className={styleAdminPanel.buttons_style}>
-              <Link className={styleAdminPanel.links_style} to='/create-event'>Crear Evento</Link>
-            </button>
-            {/* <button className={styleAdminPanel.buttons_style}>button 3</button> */}
+	return (
+		<div>
+			<div className={styleAdminPanel.title_admin}>
+				<div className={styleAdminPanel.title_style}>
+					<h1>Panel de Administrador</h1>
+				</div>
+			</div>
+			<div className={styleAdminPanel.buttons_admin}>
+				<Stack
+					direction="row"
+					divider={<Divider orientation="vertical" flexItem />}
+					spacing={2}
+				>
+					<button className={styleAdminPanel.buttons_style}>
+						<Link className={styleAdminPanel.links_style} to="/userslist">
+							Ir a la lista de usuarios
+						</Link>
+					</button>
+					<button className={styleAdminPanel.buttons_style}>
+						<Link className={styleAdminPanel.links_style} to="/events-config">
+							Configurar Eventos
+						</Link>
+					</button>
+					<button className={styleAdminPanel.buttons_style}>
+						<Link className={styleAdminPanel.links_style} to="/create-category">
+							Crear Categoria
+						</Link>
+					</button>
+					<button className={styleAdminPanel.buttons_style}>
+						<Link className={styleAdminPanel.links_style} to="/create-event">
+							Crear Evento
+						</Link>
+					</button>
+					{/* <button className={styleAdminPanel.buttons_style}>button 3</button> */}
+				</Stack>
+			</div>
+		</div>
+	);
+};
 
-      </Stack>
-        </div>
-    </div>
-  )
-}
-
-export default AdminPanel
+export default AdminPanel;
