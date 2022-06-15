cb298b8ec74899b5203b3008373a46a447c22501
unknown <lucasmontero.wed.odin@gmail.com>
2022-06-15 12:01:01 -0300

    feact(client/src/slices/app usersApiSlice)getFavorites query

--- client/src/slices/app/usersApiSlice.ts
+++ client/src/slices/app/usersApiSlice.ts
@@ -1,14 +1,18 @@
 import { apiSlice } from "../authentication/apiSlice";
-import { User } from "../../types";
+import { User, Event } from "../../types";
 
 export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: builder => ({
     getUsers: builder.query<User[],void>({
       query: () => '/api/users',
-    })
+    }),
+    getFavorites: builder.query<any,{_:string}>({
+      query: ({_}) => '/api/user/favorites',
+    }),
   })
 })
 
 export const{
-  useGetUsersQuery
+  useGetUsersQuery,
+  useGetFavoritesQuery
 } = usersApiSlice
