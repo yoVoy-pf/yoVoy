54618522ac99962831e7a1da2f6aa0f4cdc8509d
shek1ng <sheking142@gmail.com>
2022-06-16 17:45:41 -0300

    feat(client/components/UserPurchaseDetail): add component UserPurchaseDetail

---
+++ client/src/components/UserPurchaseDetail/UserPurchaseDetail.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import { useGetTicketsQuery } from '../../slices/app/usersApiSlice';
+import Tickets from '../Tickets/Tickets';
+
+const UserPurchaseDetail = () => {
+	const [tickets, setTickets] = useState<any>([]);
+	let { data, isError, isFetching } = useGetTicketsQuery(
+		{ _: '' },
+		{ refetchOnMountOrArgChange: true },
+	);
+	useEffect(() => {
+		if (!isFetching) {
+			isError ? setTickets(['No hay Tickets']) : setTickets(data);
+		}
+	}, [isFetching]);
+
+	const content = isFetching ? (
+		<h1>Cargando...</h1>
+	) : (
+		<div>
+			<h1>
+				{tickets &&
+					tickets?.length > 0 &&
+					tickets?.map((ticket: any) => {
+						if (ticket == 'No hay Tickets') {
+							return (
+								<div key="No hay Tickets">
+									<h1>No hay Tickets</h1>
+								</div>
+							);
+						} else return <Tickets ticket={ticket} />;
+					})}
+			</h1>
+		</div>
+	);
+	return <div>{content}</div>;
+};
+
+export default UserPurchaseDetail;
