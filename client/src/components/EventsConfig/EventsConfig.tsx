81b9a0ddbbadfed9f52074d12a27f7d23c2eb11f
Gonzalo Madona <gonzalomadona42@gmail.com>
2022-06-16 00:05:48 -0300

    feat(client/style): add style to table event config and button-link

--- client/src/components/EventsConfig/EventsConfig.tsx
+++ client/src/components/EventsConfig/EventsConfig.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { getAllEvent } from '../../redux/actions/actions-Create';
 import { AppDispatch, State } from '../../redux/store/store';
 import { useDeleteEventMutation } from '../../slices/app/eventsApiSlice';
@@ -26,31 +26,45 @@ const EventsConfig = () => {
 
 	return (
 		<React.Fragment>
-			<h1 style={{ textAlign: "center", color: "white" }}>Configurar Eventos</h1>
+			<div style={{ marginTop: "30px" }}>
+				<span className={styleConfigEvent.table_link}>
+					<Link className={styleConfigEvent.table_link_style} to='/admin-panel' >Volver</Link>
+				</span>
+				<div className={styleConfigEvent.table_title}>
+					<h1 className={styleConfigEvent.table_title_style} >Configurar Eventos</h1>
+				</div>
 			<table className={styleConfigEvent.table_config}>
+					<thead>
+						<tr>
+						<th style={{ textAlign: "center" }}>ID</th>
+						<th style={{ textAlign: "center" }}>Name</th>
+						<th style={{ textAlign: "center" }}>Action</th>
+						</tr>
+					</thead>
 				<tbody>
-					<tr>
-					<th>ID</th>
-					<th style={{ textAlign: "center" }}>Name</th>
-					<th style={{ textAlign: "center" }}>Action</th>
-					</tr>
 			{allEvents.map((event: any, index: any) => {
 				return (
 						<tr>
-							<th scope="row">{event.id}</th>
+							<th scope="row" style={{ textAlign: "center" }}>{event.id}</th>
 							{/* <td>{event.id}</td>	 */}
 							<td className={styleConfigEvent.th_config}>{event.name}</td>
-							<td>
-								<button onClick={() => navigate(`/update-event/${event.id}`)}>
-									Editar
+							<td className={styleConfigEvent.th_config}>
+							<Link to={`/update-event/${event.id}`} className={styleConfigEvent.buttom}>
+								<button className={styleConfigEvent.buttom_style_left}>Editar</button>
+							</Link>
+								<button 
+								className={styleConfigEvent.buttom_style_right}
+								onClick={() => HandleDelete(event)}
+								>
+									Eliminar
 								</button>
-								<button onClick={() => HandleDelete(event)}>Eliminar</button>
 							</td>
 						</tr>
 				);
 			})}
 				</tbody>
 			</table>
+			</div>
 			;
 		</React.Fragment>
 	);
