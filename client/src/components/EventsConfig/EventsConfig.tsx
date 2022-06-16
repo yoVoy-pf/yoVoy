f7f0807ca8d50ebec6e817a5bb1fff60f75a2576
Gonzalo Madona <gonzalomadona42@gmail.com>
2022-06-15 23:11:57 -0300

    refactor(client/event-config): refactor config event

--- client/src/components/EventsConfig/EventsConfig.tsx
+++ client/src/components/EventsConfig/EventsConfig.tsx
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import { getAllEvent } from '../../redux/actions/actions-Create';
 import { AppDispatch, State } from '../../redux/store/store';
 import { useDeleteEventMutation } from '../../slices/app/eventsApiSlice';
+import styleConfigEvent from './event-config.module.css';
 
 const EventsConfig = () => {
 	const dispatch: AppDispatch = useDispatch();
@@ -25,21 +26,35 @@ const EventsConfig = () => {
 
 	return (
 		<React.Fragment>
-			<h1>Configurar Eventos</h1>
-			{allEvents.map((event: any) => {
+			<h1 style={{ textAlign: "center", color: "white" }}>Configurar Eventos</h1>
+			<table className={styleConfigEvent.table_config}>
+				<tbody>
+					<tr>
+					<th>ID</th>
+					<th style={{ textAlign: "center" }}>Name</th>
+					<th style={{ textAlign: "center" }}>Action</th>
+					</tr>
+			{allEvents.map((event: any, index: any) => {
 				return (
-					<li key={event.id}>
-						{event.id} {event.name}
-						<button onClick={() => navigate(`/update-event/${event.id}`)}>
-							Editar
-						</button>
-						<button onClick={() => HandleDelete(event)}>Eliminar</button>
-					</li>
+						<tr>
+							<th scope="row">{event.id}</th>
+							{/* <td>{event.id}</td>	 */}
+							<td className={styleConfigEvent.th_config}>{event.name}</td>
+							<td>
+								<button onClick={() => navigate(`/update-event/${event.id}`)}>
+									Editar
+								</button>
+								<button onClick={() => HandleDelete(event)}>Eliminar</button>
+							</td>
+						</tr>
 				);
 			})}
+				</tbody>
+			</table>
 			;
 		</React.Fragment>
 	);
 };
 
 export default EventsConfig;
+
