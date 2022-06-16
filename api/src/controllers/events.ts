79b62c95601ce514f8a0289fe730633e29d9aa9d
Mariano Dunand <marianoo.14.md@gmail.com>
2022-06-16 19:43:31 -0300

    feat(api/events): add news controllers and utils

--- api/src/controllers/events.ts
+++ api/src/controllers/events.ts
@@ -1,23 +1,37 @@
-import { Request, Response, NextFunction } from "express"
-import { getEventsFromDb, getEventsFromDbBySearch, getEventsFromDbByFilter } from '../utils/events'
+import { Request, Response, NextFunction } from 'express';
+import {
+	getEventsFromDb,
+	getEventsFromDbBySearch,
+	getEventsFromDbByFilter,
+} from '../utils/events';
 
-export const getEvents = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const search = req.query.search as string
-        const category = req.query.category as string
-        const location = req.query.location as string
-        const city = req.query.city as string
-        const organization = req.query.organization as string
-        let events;
-        if (search) events = await getEventsFromDbBySearch(search.trim())
-        else if(category || location || organization || city ) events = await getEventsFromDbByFilter(category,location,organization, city)
-        else events = await getEventsFromDb()
+export const getEvents = async (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
+	try {
+		const search = req.query.search as string;
+		const category = req.query.category as string;
+		const location = req.query.location as string;
+		const city = req.query.city as string;
+		const organization = req.query.organization as string;
+		const name = req.query.name as string;
+		let events;
+		if (search) events = await getEventsFromDbBySearch(search.trim());
+		else if (category || location || organization || city || name)
+			events = await getEventsFromDbByFilter(
+				category,
+				location,
+				organization,
+				city,
+				name,
+			);
+		else events = await getEventsFromDb();
 
-        if(!events.length) next({status: 404, message: `Event/s not found`})
-        else res.status(200).json(events)
-
-    } catch (error) {
-        next(error)
-    }
-
-}
+		if (!events.length) next({ status: 404, message: `Event/s not found` });
+		else res.status(200).json(events);
+	} catch (error) {
+		next(error);
+	}
+};
--- api/src/utils/events.ts
+++ api/src/utils/events.ts
@@ -1,65 +1,81 @@
-import { sequelize } from '../db'
-import { Op } from "sequelize";
-const { Event, Organization, Date, Category, Location } = sequelize.models
+import { sequelize } from '../db';
+import { Op } from 'sequelize';
+const { Event, Organization, Date, Category, Location, EventLocation } =
+	sequelize.models;
 
-const attributes = ["id","name","background_image", "description"]
+const attributes = ['id', 'name', 'background_image', 'description'];
 
 //trae todos los eventos de la base de datos
 export async function getEventsFromDb() {
-    const events = await Event.findAll({attributes: attributes})
-    return events
+	const events = await Event.findAll({ attributes: attributes });
+	return events;
 }
 
 //trae los eventos de la base de datos que conincidan con la busqueda del searchBar.
 export async function getEventsFromDbBySearch(search: string) {
-    const eventsSearched = await Event.findAll(
-       { attributes: attributes,
-        where: {
-        name: {
-            [Op.iLike] : `%${search}%`
-        }
-       }
-    }
-    )
+	const eventsSearched = await Event.findAll({
+		attributes: attributes,
+		where: {
+			name: {
+				[Op.iLike]: `%${search}%`,
+			},
+		},
+	});
 
-    return eventsSearched
+	return eventsSearched;
 }
 
 //trae los eventos filtrados por categoria y locacion.
-export async function getEventsFromDbByFilter(category?: string, location?: string, organization?: string, city?: string){
-    let options: any = {include: []}
+export async function getEventsFromDbByFilter(
+	category?: string,
+	location?: string,
+	organization?: string,
+	city?: string,
+	name?: string,
+) {
+	let options: any = { include: [] };
 
-    if(organization){
-       options.include.push({
-        model: Organization,
-        where: {id: organization},
-        attributes: []
-       })
-    }
-    if(city){
-        options.include.push({
-            model: Location,
-            where: {cityId: city},
-            attributes: []
-        })
-    }
-    if(category){
-        options.include.push({
-            model: Category,
-            where: {id: category},
-            attributes: []
-        })
-    }
-    if(location && !city){
-        options.include.push({
-            model: Location,
-            where: {id: location},
-            attributes: []
-        })
-    }
+	if (organization) {
+		options.include.push({
+			model: Organization,
+			where: { id: organization },
+			attributes: [],
+		});
+	}
+	if (city) {
+		options.include.push({
+			model: Location,
+			where: { cityId: city },
+			attributes: [],
+		});
+	}
+	if (category) {
+		options.include.push({
+			model: Category,
+			where: { id: category },
+			attributes: [],
+		});
+	}
+	if (location && !city) {
+		options.include.push({
+			model: Location,
+			where: { id: location },
+			attributes: [],
+		});
+	}
+	if (name) {
+		options = {
+			where: { name: name },
+			include: [
+				{
+					model: Location,
+					include: [{ model: EventLocation, include: [{ model: Date }] }],
+				},
+			],
+		};
+	}
+	options.attributes = attributes;
+	const events = await Event.findAll(options);
 
-    options.attributes = attributes
-    const events = await Event.findAll(options)
-   
-    return events
+	return events;
 }
