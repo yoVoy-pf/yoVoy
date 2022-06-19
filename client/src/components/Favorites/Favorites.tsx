4dec991ffe55ad08e41ee29e8c033b837e085cd2
unknown <lucasmontero.wed.odin@gmail.com>
2022-06-19 20:38:03 -0300

    feat(client/components Favorites)add button and handleDelete function

--- client/src/components/Favorites/Favorites.tsx
+++ client/src/components/Favorites/Favorites.tsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from "react";
 import { useGetFavoritesQuery } from "../../slices/app/usersApiSlice"
+import { useDeleteEventToFavoriteMutation } from '../../slices/app/usersApiSlice';
 import Card from '../Card/Card';
 import style from "../Events/Events.module.css"
 import './Favorites.css'
 
 const Favorites = () => {
 
+   const [deleteEventToFavorite] = useDeleteEventToFavoriteMutation()
+
     const [favorites, setFavorites] = useState<any>([]);
-    let { data, isError,isFetching } = useGetFavoritesQuery({ _: "" }, { refetchOnMountOrArgChange: true, })
+    let { data, isError,isFetching, refetch} = useGetFavoritesQuery({ _: "" }, { refetchOnMountOrArgChange: true, })
     useEffect(() => {
     if (!isFetching){
       isError
@@ -16,13 +19,26 @@ const Favorites = () => {
     }
     }, [isFetching])
 
+    const handleDelete = async (id: any) => {
+      if (
+        window.confirm('Seguro que quieres eliminar este evento de la lista?')
+      ){
+        await deleteEventToFavorite(id)
+        refetch()
+        alert('Evento favorito eliminado correactamente')
+      }
+    }
+
     const content = isFetching
     ?  <h1>Cargando...</h1>
     : <div className={favorites[0]!=="no hay"? style.container:""}>
         {favorites && favorites?.length > 0 && favorites?.map((e: any) =>
           e==="no hay"?
           <h1 className={"text_alert"}>No hay favoritos aun</h1> :
-          <Card key={e.id} event={e} />
+          <div>
+            <button className={"deleteFav"} onClick={()=>{handleDelete(e.id)}}>X</button>
+            <Card key={e.id} event={e} />
+          </div>
         )}
       </div>
     return (
