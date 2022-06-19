6472335905f16360b90333aedc8ebe7cdf663a2a
Gonzalo Madona <gonzalomadona42@gmail.com>
2022-06-18 21:20:33 -0300

    feat(client/style): add style text

--- client/src/components/UserPurchaseDetail/UserPurchaseDetail.tsx
+++ client/src/components/UserPurchaseDetail/UserPurchaseDetail.tsx
@@ -25,7 +25,7 @@ const UserPurchaseDetail = () => {
 						if (ticket == 'No hay Tickets') {
 							return (
 								<div key="No hay Tickets">
-									<h1>No hay Tickets</h1>
+									<h1 style={{ color: 'white', textAlign: "center" }}>No hay Tickets</h1>
 								</div>
 							);
 						} else return <Tickets ticket={ticket} />;
