64a950494b514467c098e9a30f6c296d0cbc6cc7
unknown <lucasmontero.wed.odin@gmail.com>
2022-06-19 15:28:30 -0300

    refactor(client/components favorites)no favorites result show msg

--- client/src/components/Favorites/Favorites.tsx
+++ client/src/components/Favorites/Favorites.tsx
@@ -2,23 +2,28 @@ import { useEffect, useState } from "react";
 import { useGetFavoritesQuery } from "../../slices/app/usersApiSlice"
 import Card from '../Card/Card';
 import style from "../Events/Events.module.css"
+import './Favorites.css'
 
 const Favorites = () => {
 
-    const [favorites, setFavorites] = useState([]);
+    const [favorites, setFavorites] = useState<any>([]);
     let { data, isError,isFetching } = useGetFavoritesQuery({ _: "" }, { refetchOnMountOrArgChange: true, })
     useEffect(() => {
     if (!isFetching){
       isError
-      ? setFavorites([])
+      ? setFavorites(["no hay"])
       : setFavorites(data)
     }
     }, [isFetching])
 
     const content = isFetching
     ?  <h1>Cargando...</h1>
-    : <div className={style.container}>
-        {favorites && favorites?.length > 0 && favorites?.map((e: any) => <Card key={e.id} event={e} />)}
+    : <div className={favorites[0]!=="no hay"? style.container:""}>
+        {favorites && favorites?.length > 0 && favorites?.map((e: any) =>
+          e==="no hay"?
+          <h1 className={"text_alert"}>No hay favoritos aun</h1> :
+          <Card key={e.id} event={e} />
+        )}
       </div>
     return (
         <div>
