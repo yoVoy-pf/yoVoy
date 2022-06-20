e30f37311accbdaff3988bd603e5c0fb0a8cbf54
Alan <92216020+shekingw@users.noreply.github.com>
2022-06-20 20:31:16 -0300

    Revert "Refactor/favorites confirm alert"

--- client/src/components/Favorites/Favorites.tsx
+++ client/src/components/Favorites/Favorites.tsx
@@ -1,72 +1,51 @@
-import { useEffect, useState } from 'react';
-import { useGetFavoritesQuery } from '../../slices/app/usersApiSlice';
+import { useEffect, useState } from "react";
+import { useGetFavoritesQuery } from "../../slices/app/usersApiSlice"
 import { useDeleteEventToFavoriteMutation } from '../../slices/app/usersApiSlice';
 import Card from '../Card/Card';
-import style from '../Events/Events.module.css';
-import './Favorites.css';
-import Swal from 'sweetalert2';
+import style from "../Events/Events.module.css"
+import './Favorites.css'
 
 const Favorites = () => {
-	const [deleteEventToFavorite] = useDeleteEventToFavoriteMutation();
 
-	const [favorites, setFavorites] = useState<any>([]);
-	let { data, isError, isFetching, refetch } = useGetFavoritesQuery(
-		{ _: '' },
-		{ refetchOnMountOrArgChange: true },
-	);
-	useEffect(() => {
-		if (!isFetching) {
-			isError ? setFavorites(['no hay']) : setFavorites(data);
-		}
-	}, [isFetching]);
+   const [deleteEventToFavorite] = useDeleteEventToFavoriteMutation()
 
-	const handleDelete = async (id: any) => {
-		Swal.fire({
-			title: 'Esta seguro de eliminar el Evento?',
-			icon: 'warning',
-			showCancelButton: true,
-			confirmButtonColor: 'orange',
-			cancelButtonColor: '#d33',
-			cancelButtonText: 'Cancelar',
-			confirmButtonText: 'Eliminar',
-		}).then((result) => {
-			if (result.isConfirmed) {
-				Swal.fire({
-					title: 'Evento Eliminado!',
-					icon: 'success',
-				});
-				deleteEventToFavorite(id);
-				refetch();
-			}
-		});
-	};
+    const [favorites, setFavorites] = useState<any>([]);
+    let { data, isError,isFetching, refetch} = useGetFavoritesQuery({ _: "" }, { refetchOnMountOrArgChange: true, })
+    useEffect(() => {
+    if (!isFetching){
+      isError
+      ? setFavorites(["no hay"])
+      : setFavorites(data)
+    }
+    }, [isFetching])
 
-	const content = isFetching ? (
-		<h1>Cargando...</h1>
-	) : (
-		<div className={favorites[0] !== 'no hay' ? style.container : ''}>
-			{favorites &&
-				favorites?.length > 0 &&
-				favorites?.map((e: any) =>
-					e === 'no hay' ? (
-						<h1 className={'text_alert'}>No hay favoritos aun</h1>
-					) : (
-						<div>
-							<button
-								className={'deleteFav'}
-								onClick={() => {
-									handleDelete(e.id);
-								}}
-							>
-								X
-							</button>
-							<Card key={e.id} event={e} />
-						</div>
-					),
-				)}
-		</div>
-	);
-	return <div>{content}</div>;
-};
+    const handleDelete = async (id: any) => {
+      if (
+        window.confirm('Seguro que quieres eliminar este evento de la lista?')
+      ){
+        await deleteEventToFavorite(id)
+        refetch()
+        alert('Evento favorito eliminado correactamente')
+      }
+    }
 
-export default Favorites;
+    const content = isFetching
+    ?  <h1>Cargando...</h1>
+    : <div className={favorites[0]!=="no hay"? style.container:""}>
+        {favorites && favorites?.length > 0 && favorites?.map((e: any) =>
+          e==="no hay"?
+          <h1 className={"text_alert"}>No hay favoritos aun</h1> :
+          <div>
+            <button className={"deleteFav"} onClick={()=>{handleDelete(e.id)}}>X</button>
+            <Card key={e.id} event={e} />
+          </div>
+        )}
+      </div>
+    return (
+        <div>
+          {content}
+        </div>
+    )
+}
+
+export default Favorites
