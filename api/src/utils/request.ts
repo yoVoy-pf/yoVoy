5a1318890940e4724b1d310f538aa95f82623abc
Avila Diego <aviladiego2009@gmail.com>
2022-06-22 11:27:13 -0300

    feat(api/utils/routes): added new function executeRequest

--- api/src/utils/request.ts
+++ api/src/utils/request.ts
@@ -1,16 +1,20 @@
 import { sequelize } from "../db";
+import { Model } from "sequelize";
 import fs from "fs"
 const { Request } = sequelize.models
 
+enum list{
+    POST_organization = "createOrganization"
+}
+
 export const createRequest = async (userId: number, description: string, type: string, method: string, information: any) => {
-
-    const request  = await Request.create({userId, description, type, method})
-
+    const request  = await Request.create({userId, description, type, method, function_type: list[`${method}_${type}` as keyof typeof list]})
+    
     if (!fs.existsSync("./requests")){
         fs.mkdirSync("./requests");
     }
-
-    const url_body = `./requests/${request.getDataValue("id")}-${method}-${type}-.json`
+    const id = request.getDataValue("id")
+    const url_body = `./requests/${id}_${method}_${type}.json`
 
     fs.writeFile(url_body,JSON.stringify(information), "utf-8", (err) => {
         if(err) throw err;
@@ -19,8 +23,24 @@ export const createRequest = async (userId: number, description: string, type: s
     request.update({url_body})
 }
 
+const executeRequest = (request: Model<any,any>) => {
+    const id = request.getDataValue("id")
+    const type = request.getDataValue("type")
+    const method = request.getDataValue("method")
+    const function_type = request.getDataValue("function_type")
+
+    let body = fs.readFileSync(`./requests/${id}_${method}_${type}.json`, {encoding:'utf8', flag:'r'} )
+    body = JSON.parse(body)
+    
+    require(`../utils/${type}.ts`)[function_type](body)
+}
+
 export const updateRequest = async (id:string | number, status: string) => {
     let request = await Request.findByPk(id)
+    if(request){
 
-    request?.update({status})
+        request.update({status})
+        
+        if(status === "accepted") executeRequest(request)
+    }
 }
