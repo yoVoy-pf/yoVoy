0343b28020d1f6acad87bc7046da431077d5784d
shek1ng <sheking142@gmail.com>
2022-06-23 20:38:33 -0300

    feat(client/components/CreateCategory) add alert createCategory

--- client/src/components/CreateCategory/CreateCategory.tsx
+++ client/src/components/CreateCategory/CreateCategory.tsx
@@ -1,58 +1,94 @@
-import { useState } from "react"
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import { useCreateCategoryMutation } from "../../slices/app/categoriesApiSlice";
-import SideBar from "../SideBar/SideBar";
-import styleCreateCategory from './create-category.module.css'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useCreateCategoryMutation } from '../../slices/app/categoriesApiSlice';
+import SideBar from '../SideBar/SideBar';
+import styleCreateCategory from './create-category.module.css';
+import Swal from 'sweetalert2';
 
 const CreateCategory = () => {
-    const navigate = useNavigate()
+	const Toast = Swal.mixin({
+		toast: true,
+		position: 'top-end',
+		showConfirmButton: false,
+		timer: 3000,
+		timerProgressBar: true,
+		didOpen: (toast) => {
+			toast.addEventListener('mouseenter', Swal.stopTimer);
+			toast.addEventListener('mouseleave', Swal.resumeTimer);
+		},
+	});
+	const navigate = useNavigate();
 
-    const [createCategory] = useCreateCategoryMutation();
-    const [category, setCategory] = useState({
-        name: ''
-    });
-    
-    const onInputChange = (e:any) => {
-        e.preventDefault();
-        setCategory({
-          ...category,
-          [e.target.name]: e.target.value,
-        });
-    }
+	const [createCategory] = useCreateCategoryMutation();
+	const [category, setCategory] = useState({
+		name: '',
+	});
 
-    const onSubmit = async (e:any) => {
-        e.preventDefault();
-        console.log(category.name)
-        if (category) await createCategory({category: category.name})
-        setCategory({
-            name: ''
-        })
-        navigate('/')
-    }
-    return(
-        <div className={styleCreateCategory.fondo}>
-            <SideBar/>
-            <form onSubmit={onSubmit}>
-                <div className={styleCreateCategory.form_create_category}>
-                <fieldset>
-                    {/* <label>Nombre de la Categoria</label> <br /> */}
-                    <legend className={styleCreateCategory.legend_create_category}>Nombre de la Categoria:</legend>
-                    <input 
-                        type="text" 
-                        placeholder="Nombre"
-                        name='name'
-                        required
-                        className={styleCreateCategory.input_create_categoty}
-                        onChange={onInputChange}
-                        value={category.name}
-                    /> 
-                </fieldset> <br />
-                <button className={styleCreateCategory.buttom_create_category} type="submit">¡Crear Categoria!</button>
-                </div>
-            </form>
-        </div>
-    )
-}
+	const onInputChange = (e: any) => {
+		e.preventDefault();
+		setCategory({
+			...category,
+			[e.target.name]: e.target.value,
+		});
+	};
 
-export default CreateCategory
+	const onSubmit = async (e: any) => {
+		e.preventDefault();
+		Swal.fire({
+			title: `Crear Categoría ${category.name}?`,
+			icon: 'warning',
+			showCancelButton: true,
+			confirmButtonColor: 'orange',
+			cancelButtonColor: '#d33',
+			cancelButtonText: 'Cancelar',
+			confirmButtonText: 'Crear',
+		}).then(async (result) => {
+			if (result.isConfirmed) {
+				Toast.fire({
+					title: `Caterogía creada!`,
+					icon: 'success',
+				});
+				console.log(category.name);
+				if (category) await createCategory({ category: category.name });
+				setCategory({
+					name: '',
+				});
+				navigate('/');
+			}
+		});
+	};
+	return (
+		<div className={styleCreateCategory.fondo}>
+			<SideBar />
+			<form onSubmit={onSubmit}>
+				<div className={styleCreateCategory.form_create_category}>
+					<fieldset>
+						{/* <label>Nombre de la Categoria</label> <br /> */}
+						<legend className={styleCreateCategory.legend_create_category}>
+							Nombre de la Categoria:
+						</legend>
+						<input
+							type="text"
+							placeholder="Nombre"
+							name="name"
+							required
+							className={styleCreateCategory.input_create_categoty}
+							onChange={onInputChange}
+							value={category.name}
+						/>
+					</fieldset>{' '}
+					<br />
+					<button
+						className={styleCreateCategory.buttom_create_category}
+						type="submit"
+					>
+						¡Crear Categoria!
+					</button>
+				</div>
+			</form>
+		</div>
+	);
+};
+
+export default CreateCategory;
