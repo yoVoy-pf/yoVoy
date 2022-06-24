12e6a8e173e7b12309450ccd603bef2dcddf4de0
Avila Diego <aviladiego2009@gmail.com>
2022-06-24 01:52:48 -0300

    feat(api/cities): added paginate

--- api/src/controllers/cities.ts
+++ api/src/controllers/cities.ts
@@ -4,7 +4,7 @@ import { getCitiesFromDb } from '../utils/cities'
 export const getCities = async (req: Request, res: Response, next: NextFunction) => {
 
     try {
-        const cities = await getCitiesFromDb();
+        const cities = await getCitiesFromDb(req.body.paginate);
 
         if(!cities.length) next({status:404, message: "Cities not found"})
         else res.status(200).send(cities)
