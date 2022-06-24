c77f1dd6f33d55e7b09342d0717de62681f30914
shek1ng <sheking142@gmail.com>
2022-06-24 13:22:06 -0300

    refactor(client/components/SearchBar): add alert error to failed search

--- client/src/components/SearchBar/SearchBar.tsx
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -1,37 +1,57 @@
-import React, { useState } from "react";
-import { AppDispatch } from "../../redux/store/store";
-import { useDispatch } from "react-redux";
-import { getSearchEvent } from "../../redux/actions/actions-Create";
-import "./SearchBar.css"
+import React, { useState } from 'react';
+import { AppDispatch } from '../../redux/store/store';
+import { useDispatch } from 'react-redux';
+import { getSearchEvent } from '../../redux/actions/actions-Create';
+import './SearchBar.css';
+import Swal from 'sweetalert2';
 
 type FormElement = React.FormEvent<HTMLFormElement>;
-type Input = React.ChangeEvent<HTMLInputElement>
+type Input = React.ChangeEvent<HTMLInputElement>;
 
 const SearchBar = () => {
-    const dispatch: AppDispatch = useDispatch();
-    const [input, setInput] = useState("");
+	const Toast = Swal.mixin({
+		toast: true,
+		position: 'top-end',
+		showConfirmButton: false,
+		timer: 1500,
+		timerProgressBar: true,
+		didOpen: (toast) => {
+			toast.addEventListener('mouseenter', Swal.stopTimer);
+			toast.addEventListener('mouseleave', Swal.resumeTimer);
+		},
+	});
+	const dispatch: AppDispatch = useDispatch();
+	const [input, setInput] = useState('');
 
-    function onSubmit(e: FormElement) {
-        e.preventDefault();
-        input ? dispatch(getSearchEvent(input)) : alert("oops! empty field");
-        setInput("");
-    };
+	function onSubmit(e: FormElement) {
+		e.preventDefault();
+		input
+			? dispatch(getSearchEvent(input))
+			: Toast.fire({
+					title: 'Ingrese el nombre de un Evento',
+					icon: 'warning',
+			  });
+		setInput('');
+	}
 
-    function onInputChange(e: Input) {
-        setInput(e.target.value)
-    }
+	function onInputChange(e: Input) {
+		setInput(e.target.value);
+	}
 
-    return (
-        <React.Fragment>
-            
-                <form onSubmit={onSubmit}>
-                    <input className="searchBar-bg-input1"  type="text" onChange={onInputChange} value={input} placeholder="Buscar..." />
-                    <input className="searchBar-bg-input2" type="submit" value="🔍" />
-                </form>
-          
-
-        </React.Fragment>
-    )
-}
+	return (
+		<React.Fragment>
+			<form onSubmit={onSubmit}>
+				<input
+					className="searchBar-bg-input1"
+					type="text"
+					onChange={onInputChange}
+					value={input}
+					placeholder="Buscar..."
+				/>
+				<input className="searchBar-bg-input2" type="submit" value="🔍" />
+			</form>
+		</React.Fragment>
+	);
+};
 
 export default SearchBar;
