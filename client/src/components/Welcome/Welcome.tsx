7c101195ccd8f9d293d9e23bf389ef120abe0d81
Federico A. Gomez <fmtgomez@gmail.com>
2022-06-24 22:14:19 -0300

    feat(client/welcome):update style welcome

--- client/src/components/Welcome/Welcome.tsx
+++ client/src/components/Welcome/Welcome.tsx
@@ -5,6 +5,11 @@ import {
 } from '../../slices/authentication/authSlice';
 import { Link } from 'react-router-dom';
 import styleWelcome from './welcome.module.css';
+import adminImg from "../../img/admin.jpg"
+import cambiarPass from "../../img/cambiarpass.jpg"
+import detalleCuenta from "../../img/detallecuenta.jpg"
+import crearOrg from "../../img/crearorg.jpg"
+import eventos from "../../img/eventos.jpg"
 
 export const Welcome = () => {
 	const user: any = useSelector(selectCurrentUser);
@@ -29,71 +34,129 @@ export const Welcome = () => {
 			{/* <p>Token: {token}</p> */}
 			{currentRole[0] === 3030 && (
 				<div className={styleWelcome.links_welcome}>
-					<div className={styleWelcome.order_welcome}>
-						<p>
-							<Link className={styleWelcome.links_style} to="/userslist">
-								Ir al panel de admin
-							</Link>
-						</p>
-            <p>
-              <Link className={styleWelcome.links_style} to="/change-password">
-                Cambiar contraseña
-              </Link>
-            </p>
-						<p>
-							<Link className={styleWelcome.links_style} to="/user/information">
-								Detalles de Cuenta
-							</Link>
-						</p>
-					</div>
+
+					{/*Estilo para el Admin */}
+					<Link className={styleWelcome.links_style} to="/userslist">
+						<div className={styleWelcome.containerImgAndH1}>
+							<img src={adminImg} alt="adminImg" />
+							<div className={styleWelcome.containerH1}>
+								<div>
+									<h1>Ir al panel de Administrador</h1>
+								</div>
+							</div>
+						</div>
+					</Link>
+
+
+					<Link className={styleWelcome.links_style} to="/change-password">
+						<div className={styleWelcome.containerImgAndH1}>
+							<img src={cambiarPass} alt="cambiarPass" />
+							<div className={styleWelcome.containerH1}>
+								<div>
+									<h1>Cambiar Contraseña</h1>
+								</div>
+							</div>
+						</div>
+					</Link>
+
+
+					<Link className={styleWelcome.links_style} to="/user/information">
+						<div className={styleWelcome.containerImgAndH1}>
+							<img src={detalleCuenta} alt="detalleCuenta" />
+							<div className={styleWelcome.containerH1} >
+								<div>
+									<h1>Detalle de cuenta</h1>
+								</div>
+							</div>
+						</div>
+					</Link>
+
+
 				</div>
 			)}
 			{currentRole[0] === 1010 && (
 				<div className={styleWelcome.links_welcome}>
-					<div className={styleWelcome.order_welcome}>
-						<p>
-							<Link
-								className={styleWelcome.links_style}
-								to="/create-organization"
-							>
-								Crear Organización
-							</Link>
-						</p>
-            <p>
-              <Link className={styleWelcome.links_style} to="/change-password">
-                Cambiar contraseña
-              </Link>
-            </p>
-						<p>
-							<Link className={styleWelcome.links_style} to="/user/information">
-								Detalles de Cuenta
-							</Link>
-						</p>
-					</div>
+
+
+					<Link className={styleWelcome.links_style} to="/create-organization">
+						<div className={styleWelcome.containerImgAndH1}>
+							<img src={crearOrg} alt="cambiarPass" />
+							<div className={styleWelcome.containerH1}>
+								<div>
+									<h1>Crear Organización</h1>
+								</div>
+							</div>
+						</div>
+					</Link>
+
+
+					<Link className={styleWelcome.links_style} to="/change-password">
+						<div className={styleWelcome.containerImgAndH1}>
+							<img src={cambiarPass} alt="cambiarPass" />
+							<div className={styleWelcome.containerH1}>
+								<div>
+									<h1>Cambiar Contraseña</h1>
+								</div>
+							</div>
+						</div>
+					</Link>
+
+
+					<Link className={styleWelcome.links_style} to="/user/information">
+						<div className={styleWelcome.containerImgAndH1}>
+							<img src={detalleCuenta} alt="detalleCuenta" />
+							<div className={styleWelcome.containerH1} >
+								<div>
+									<h1>Detalle de cuenta</h1>
+								</div>
+							</div>
+						</div>
+					</Link>
+
+
 				</div>
 			)}
 			{currentRole[0] === 2020 && (
 				<div className={styleWelcome.links_welcome}>
-					<div className={styleWelcome.order_welcome}>
-						<p>
-							<Link
-								className={styleWelcome.links_style}
-								to="/organization-events"
-							>
-								Eventos de la organización
-							</Link>
-						</p>
-						<p>
-							<Link className={styleWelcome.links_style} to="/change-password">
-								Cambiar contraseña
-							</Link>
-						</p>
-						<p>
-							<Link className={styleWelcome.links_style} to="/user/information">
-								Detalles de Cuenta
-							</Link>
-						</p>
-					</div>
+
+
+					<Link className={styleWelcome.links_style} to="/organization-events">
+						<div className={styleWelcome.containerImgAndH1}>
+							<img src={eventos} alt="eventos" />
+							<div className={styleWelcome.containerH1}>
+								<div>
+									<h1>Eventos de la organización</h1>
+								</div>
+							</div>
+						</div>
+
+					</Link>
+
+
+					<Link className={styleWelcome.links_style} to="/change-password">
+						<div className={styleWelcome.containerImgAndH1}>
+							<img src={cambiarPass} alt="cambiarPass" />
+							<div className={styleWelcome.containerH1}>
+								<div>
+									<h1>Cambiar Contraseña</h1>
+								</div>
+							</div>
+						</div>
+					</Link>
+
+
+					<Link className={styleWelcome.links_style} to="/user/information">
+						<div className={styleWelcome.containerImgAndH1}>
+							<img src={detalleCuenta} alt="detalleCuenta" />
+							<div className={styleWelcome.containerH1} >
+								<div>
+									<h1>Detalle de cuenta</h1>
+								</div>
+							</div>
+						</div>
+					</Link>
+
+
 				</div>
 			)}
 		</section>
