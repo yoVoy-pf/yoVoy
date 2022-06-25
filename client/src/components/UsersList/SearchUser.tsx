4e7937a68d444e9c5224ee8f43705da8959bfc03
Federico A. Gomez <fmtgomez@gmail.com>
2022-06-25 18:51:45 -0300

    feat(client/paged):added style pagination

--- client/src/components/UsersList/SearchUser.tsx
+++ client/src/components/UsersList/SearchUser.tsx
@@ -1,3 +1,4 @@
+import styleUserList from './user-list.module.css';
 type FormElement = React.FormEvent<HTMLFormElement>;
 type Input = React.ChangeEvent<HTMLInputElement>
 
@@ -23,7 +24,7 @@ const SearchUser = ({email, setEmail, searchUserQuery}: any) => {
                     <input className="searchBar-bg-input1"  type="text" onChange={onInputChange} value={email} placeholder="Buscar por email..." />
                     <input className="searchBar-bg-input2" type="submit" value="🔍" />
             </form>
-            <button onClick={handleClear}>Clear</button>
+            <button className={styleUserList.buttom_style} onClick={handleClear}>Clear</button>
         </div>
     )
 }
