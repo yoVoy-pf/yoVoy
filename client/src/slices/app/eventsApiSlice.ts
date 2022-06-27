cfa18486b92594b0f94a1c3ef55c586bbdd61ba1
shek1ng <sheking142@gmail.com>
2022-06-27 15:38:20 -0300

    refactor(client/slices/eventsApiSlice) refactor getEvents for error

--- client/src/slices/app/eventsApiSlice.ts
+++ client/src/slices/app/eventsApiSlice.ts
@@ -1,64 +1,64 @@
-import { apiSlice } from "../authentication/apiSlice";
-import { Event } from "../../types";
+import { apiSlice } from '../authentication/apiSlice';
+import { Event } from '../../types';
 
 export const eventsApiSlice = apiSlice.injectEndpoints({
-  endpoints: builder => ({
-    updateEvent: builder.mutation<Event,{id: string, updatedEvent: any}>({
-      query: ({id, updatedEvent}) => {
-        // do something
-        return{
-          url: `/api/event/${id}`,
-          method:`PUT`,
-          body: {...updatedEvent}
-        }
-      }
-    }),
-    getEvents: builder.query<Event[], { _: string }>({
+	endpoints: (builder) => ({
+		updateEvent: builder.mutation<Event, { id: string; updatedEvent: any }>({
+			query: ({ id, updatedEvent }) => {
+				// do something
+				return {
+					url: `/api/event/${id}`,
+					method: `PUT`,
+					body: { ...updatedEvent },
+				};
+			},
+		}),
+		getEvents: builder.query<Event[] | any, { _: string }>({
 			query: ({ _ }) => '/api/events',
 		}),
-    addEventToFavorite: builder.mutation<Event,{eventId: string}>({
-      query: ({eventId}) => {
-        // do something
-        return{
-          url: `api/user/favorite/${eventId}`,
-          method:`POST`,
-        }
-      }
-    }),
-    createEvent: builder.mutation<Event,{newEvent: any}>({
-      query: ({newEvent}) => {
-        // do something
-        return{
-          url: `/api/event`,
-          method:`POST`,
-          body: {...newEvent}
-        }
-      }
-    }),
-    getEvent: builder.query<any,{id: string}>({
-      query: ({id}) => {
-      console.log({id})
-      return {
-        url: `api/event/${id}`
-      }
-      }
-    }),
-    deleteEvent: builder.mutation<Event, {id: number}>({
-      query(id) {
-        return {
-          url:`api/event/${id}`,
-          method:`Delete`,
-        }
-      }
-    })
-  })
-})
+		addEventToFavorite: builder.mutation<Event, { eventId: string }>({
+			query: ({ eventId }) => {
+				// do something
+				return {
+					url: `api/user/favorite/${eventId}`,
+					method: `POST`,
+				};
+			},
+		}),
+		createEvent: builder.mutation<Event, { newEvent: any }>({
+			query: ({ newEvent }) => {
+				// do something
+				return {
+					url: `/api/event`,
+					method: `POST`,
+					body: { ...newEvent },
+				};
+			},
+		}),
+		getEvent: builder.query<any, { id: string }>({
+			query: ({ id }) => {
+				console.log({ id });
+				return {
+					url: `api/event/${id}`,
+				};
+			},
+		}),
+		deleteEvent: builder.mutation<Event, { id: number }>({
+			query(id) {
+				return {
+					url: `api/event/${id}`,
+					method: `Delete`,
+				};
+			},
+		}),
+	}),
+});
 
-export const{
-  useUpdateEventMutation,
-  useCreateEventMutation,
-  useGetEventQuery,
-  useGetEventsQuery,
-  useDeleteEventMutation,
-  useAddEventToFavoriteMutation,
-} = eventsApiSlice
+export const {
+	useUpdateEventMutation,
+	useCreateEventMutation,
+	useGetEventQuery,
+	useGetEventsQuery,
+	useDeleteEventMutation,
+	useAddEventToFavoriteMutation,
+} = eventsApiSlice;
