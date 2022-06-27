361711b9d55d7d7b0e7baf7779a29661b74575cb
shek1ng <sheking142@gmail.com>
2022-06-27 15:39:27 -0300

    feat(client/components/SearchBar.tsx) add autocomplete search bar

--- client/src/components/SearchBar/SearchBar.tsx
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux';
 import { getSearchEvent } from '../../redux/actions/actions-Create';
 import './SearchBar.css';
 import Swal from 'sweetalert2';
+import { useGetEventsQuery } from '../../slices/app/eventsApiSlice';
 
 type FormElement = React.FormEvent<HTMLFormElement>;
 type Input = React.ChangeEvent<HTMLInputElement>;
@@ -20,18 +21,24 @@ const SearchBar = () => {
 			toast.addEventListener('mouseleave', Swal.resumeTimer);
 		},
 	});
+	const {
+		data: events,
+		refetch,
+		isFetching,
+	} = useGetEventsQuery({ _: '' }, { refetchOnMountOrArgChange: true });
+	const eventos = events?.rows?.map((e: any) => e);
 	const dispatch: AppDispatch = useDispatch();
 	const [input, setInput] = useState('');
 
-	function onSubmit(e: FormElement) {
-		e.preventDefault();
+	function onSubmit(searchTerm: any) {
 		input
 			? dispatch(getSearchEvent(input))
 			: Toast.fire({
 					title: 'Ingrese el nombre de un Evento',
 					icon: 'warning',
 			  });
-		setInput('');
+		setInput(searchTerm);
+		console.log('asdmasmdasmd', input);
 	}
 
 	function onInputChange(e: Input) {
@@ -40,7 +47,7 @@ const SearchBar = () => {
 
 	return (
 		<React.Fragment>
-			<form onSubmit={onSubmit}>
+			<div>
 				<input
 					className="searchBar-bg-input1"
 					type="text"
@@ -48,8 +55,35 @@ const SearchBar = () => {
 					value={input}
 					placeholder="Buscar..."
 				/>
-				<input className="searchBar-bg-input2" type="submit" value="🔍" />
-			</form>
+				<button
+					className="searchBar-bg-input2"
+					type="submit"
+					onClick={() => onSubmit(input)}
+				>
+					🔍
+				</button>
+				<div className="searchBar-autoSuggest">
+					{eventos
+						?.filter((item: any) => {
+							const searchTerm = input.toLowerCase();
+							const name = item.name.toLowerCase();
+
+							return (
+								searchTerm && name.startsWith(searchTerm) && name !== searchTerm
+							);
+						})
+						.slice(0, 3)
+						.map((e: any) => (
+							<div
+								className="searchBar-autoSuggest-row"
+								onClick={() => onSubmit(e.name)}
+								key={e.name}
+							>
+								{e.name}
+							</div>
+						))}
+				</div>
+			</div>
 		</React.Fragment>
 	);
 };
