0e94140b20461ce629db2498381cad0c49842443
unknown <lucasmontero.wed.odin@gmail.com>
2022-06-28 15:19:55 -0300

    refact(client/components createEvents,updateEvents)fieldsets rearranged, modal removed, validations enabled

--- client/src/components/CreateEvent/CreateEvent.tsx
+++ client/src/components/CreateEvent/CreateEvent.tsx
@@ -1,4 +1,4 @@
-import React, { SyntheticEvent, useEffect, useRef } from 'react';
+import React, { SyntheticEvent, useEffect, useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import {
@@ -14,8 +14,12 @@ import { useCreateEventMutation } from '../../slices/app/eventsApiSlice';
 import styleCreateEvent from './create-event.module.css';
 import { FiEdit } from 'react-icons/fi';
 import styleUpdate from "../UpdateEvent/update-event.module.css"
+import { Toast } from '../../utils/alerts';
+import { useNavigate } from 'react-router-dom';
 
 const CreateEvent = () => {
+
+	type Input = React.ChangeEvent<HTMLInputElement>
 	const locSelect = useRef<any>()
 	const [isOpen, openModal, closeModal] = useDatesModal(false);
 	const [createEvent] = useCreateEventMutation();
@@ -27,6 +31,8 @@ const CreateEvent = () => {
 	const categories: any = useSelector(
 		(state: State) => state.global.categories,
 	);
+
+	const navigate = useNavigate()
 
 	useEffect(() => {
 		dispatch(getLocations());
@@ -50,7 +56,8 @@ const CreateEvent = () => {
 		handleRemoveLoc,
 		handleUpdateFetch,
 		removeDateFromLocsAux,
-		setCurrentLocId
+		setCurrentLocId,
+		removeDateFromLocsAux2
 	] = useCreateEvent({ locations });
 
 	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -60,12 +67,152 @@ const CreateEvent = () => {
 			locations: locsForSubmit,
 		}
 		try {
-			await createEvent({ newEvent: event });
-			resetState();
+			if (err.nameErr === "" &&
+				err.descriptionErr === "" &&
+				err.categoriesErr === "" &&
+				err.locationsErr === "" &&
+				err.imgErr === "") {
+				await createEvent({ newEvent: event });
+				resetState();
+				navigate("/organization-events")
+				Toast.fire({
+					icon: "success",
+					title: "Evento creado correctamente",
+				})
+
+			} else {
+				Toast.fire({
+					icon: "warning",
+					title: "Debe completar los campos correctamente para poder crear un evento.",
+				})
+			}
+
 		} catch (error) {
 			console.log(error);
 		}
 	};
+
+	const [err, setErr] = useState({
+		nameErr: "Debe tener entre 4 a 50 caracteres",
+		descriptionErr: "Debe tener entre 50 a 250 caracteres",
+		imgErr: "Este campo debe esta completo.",
+		categoriesErr: "Debe tener al menos 1 categoria",
+		locationsErr: "Debe tener al menos 1 locación"
+	})
+
+	const [datesErr, setdatesErr] = useState({
+		priceErr: "Debe ingresar un precio",
+		locationDateErr: "Debe selecionar una locacion",
+		dateInputErr: "Debe selecionar una fecha",
+		ticketInputErr: "Debe ingresar numero de tickets",
+	})
+
+	useEffect(() => {
+		if (input.name.length < 4) {
+			setErr({ ...err, nameErr: "Debe tener entre 4 a 50 caracteres" })
+		}
+		if (input.name.length > 3) {
+			setErr({ ...err, nameErr: "" })
+		}
+	}, [input.name])
+
+	useEffect(() => {
+		if (input.description.length < 50) {
+			setErr({ ...err, descriptionErr: "Debe tener entre 50 a 250 caracteres" })
+		}
+		if (input.description.length > 49) {
+			setErr({ ...err, descriptionErr: "" })
+		}
+	}, [input.description])
+
+	useEffect(() => {
+		if (input.categories.length === 0) {
+			setErr({ ...err, categoriesErr: "Debe tener al menos 1 categoria" })
+		}
+		if (input.categories.length > 0) {
+			setErr({ ...err, categoriesErr: "" })
+		}
+	}, [input.categories])
+
+	useEffect(() => {
+		if (locsForSubmit.length === 0) {
+			setErr({ ...err, locationsErr: "Debe tener al menos 1 locación" })
+		}
+		if (locsForSubmit.length > 0) {
+			setErr({ ...err, locationsErr: "" })
+		}
+	}, [locsForSubmit])
+
+
+	useEffect(() => {
+		if (input.background_image === "") {
+			setErr({ ...err, imgErr: "Este campo debe esta completo." })
+		} else if (input.background_image !== "") {
+			if (!(/\.(jpg|png|gif)$/i).test(input.background_image)) {
+				setErr({ ...err, imgErr: "Url no contiene un archivo valido" })
+			} else {
+				setErr({ ...err, imgErr: "" })
+			}
+
+		}
+	}, [input.background_image])
+
+	useEffect(() => {
+		console.log(currentDate.price)
+		if (isNaN(currentDate.price) || currentDate.price === 0) {
+			setdatesErr({ ...datesErr, priceErr: "Debe ingresar un precio" })
+		} else if (currentDate.price > 0) {
+			setdatesErr({ ...datesErr, priceErr: "" })
+		}
+	}, [currentDate.price])
+
+
+	useEffect(() => {
+		if (currentLocId === "default" || currentLocId === "") {
+			setdatesErr({ ...datesErr, locationDateErr: "Debe selecionar una locacion" })
+		} else if (currentLocId !== "default") {
+			setdatesErr({ ...datesErr, locationDateErr: "" })
+		}
+	}, [currentLocId])
+
+	useEffect(() => {
+		if (!currentDate.date) {
+			setdatesErr({ ...datesErr, dateInputErr: "Debe selecionar una fecha" })
+		} else {
+			setdatesErr({ ...datesErr, dateInputErr: "" })
+		}
+	}, [currentDate.date])
+
+	useEffect(() => {
+		if (!currentDate.total_tickets) {
+			setdatesErr({ ...datesErr, ticketInputErr: "Debe ingresar numero de tickets" })
+		} else {
+			setdatesErr({ ...datesErr, ticketInputErr: "" })
+		}
+	}, [currentDate.total_tickets])
+
+	const handleAddDateClick = (e: any) => {
+
+		if (datesErr.priceErr === "" &&
+			datesErr.locationDateErr === "" &&
+			datesErr.dateInputErr === "" &&
+			datesErr.ticketInputErr === "" &&
+			currentLocId !== "") {
+			handleAddDate(e)
+			setdatesErr({
+				priceErr: "Debe ingresar un precio",
+				locationDateErr: "Debe selecionar una locacion",
+				dateInputErr: "Debe selecionar una fecha",
+				ticketInputErr: "Debe ingresar numero de tickets",
+			})
+		} else {
+			Toast.fire({
+				icon: "warning",
+				title: "Debe completar los datos correspondientes para poder agregar.",
+			})
+		}
+
+	}
 
 	return (
 		<React.Fragment>
@@ -85,6 +232,7 @@ const CreateEvent = () => {
 							onChange={handleInputChange}
 							value={input.name}
 						/>
+						<label>{err.nameErr}</label>
 					</fieldset>{' '}
 					<br />
 					<fieldset className={styleCreateEvent.fieldset_form}>
@@ -100,6 +248,7 @@ const CreateEvent = () => {
 							onChange={handleInputChange}
 							value={input.description}
 						/>
+						<label>{err.descriptionErr}</label>
 					</fieldset>{' '}
 					<br />
 					<fieldset className={styleCreateEvent.fieldset_form}>
@@ -114,6 +263,7 @@ const CreateEvent = () => {
 							onChange={handleInputChange}
 							value={input.background_image}
 						/>
+						<label>{err.imgErr}</label>
 					</fieldset>{' '}
 					<br />
 					<fieldset className={styleCreateEvent.fieldset_form}>
@@ -133,64 +283,19 @@ const CreateEvent = () => {
 								</React.Fragment>
 							);
 						})}
-					</fieldset>{' '}
-					<fieldset className={styleCreateEvent.fieldset_form}>
-						<legend className={styleCreateEvent.legend_form}>
-							Localidades agregadas:
-						</legend>
-						{
-							locsForSubmit.length > 0
-								? locsForSubmit
-									.map((loc: any) => {
-										let locData = locations?.rows?.find((location: any) => location.id === parseInt(loc.id));
-										return locData ?
-											(
-												<div>
-													<span>{` ${locData.name} `}</span>
-													<span>{` ${locData.address} `}</span>
-													{/* <button onClick={(e) => handleRemoveLoc(e,loc.id)}>X</button> */}
-													<button
-														onClick={() => {
-															setCurrentLocId(loc.id)
-															openModal()
-														}}
-														type="button"
-														className={styleCreateEvent.text_from}
-													>
-														<FiEdit />
-													</button>
-													<ul>
-														{loc.dates.map((date: any) => (
-															<li key={date.date}>{`$${date.price} || ${date.date}`}</li>
-														))}
-													</ul>
-												</div>
-											)
-											: null
-									})
-								: <h1>No hay localidades cargadas para el evento</h1>
-						}
+						<br />
+						<label>{err.categoriesErr}</label>
 					</fieldset>{' '}
 					<br />
 					{/* MODAL */}
 					<fieldset className={styleCreateEvent.fieldset_form}>
 						<legend className={styleCreateEvent.legend_form}>
-							Agrege detalles de el/los eventos
+							Agregue detalles de el/los eventos
 						</legend>
-						<button
-							onClick={() => openModal()}
-							type="button"
-							className={styleCreateEvent.text_from}
-						>
-							+
-						</button>
-						<DatesModal
-							isOpen={isOpen}
-							closeModal={closeModal}
-							className={styleCreateEvent.form_dates_modal}
-						>
+						<div className={styleCreateEvent.dateContainerForm}>
 							<div className={styleUpdate.container_modal}>
 								<select
+									id="locationSelect"
 									name="id"
 									ref={locSelect}
 									placeholder="Seleccione un lugar"
@@ -212,58 +317,92 @@ const CreateEvent = () => {
 										);
 									})}
 								</select>
+								<label>{datesErr.locationDateErr}</label>
 							</div>
+
 							<div className={styleUpdate.container_pricedate}>
+								<legend className={styleCreateEvent.legend_detail_form}>
+									Precio
+								</legend>
 								<input
+								    className={styleCreateEvent.input_detail_form}
 									name="price"
 									type="number"
-									value={currentDate?.price || '0'}
+									value={currentDate?.price || ""}
 									placeholder="Indique el precio..."
 									onChange={handleInputDateChange}
 								/>
+								<label>{datesErr.priceErr}</label>
+
+
+								<legend className={styleCreateEvent.legend_detail_form}>
+									Fecha
+								</legend>
 								<input type="date" name="date" value={currentDate?.date || ''} onChange={handleInputDateChange} />
-								<button type="button" onClick={handleAddDate}>
+								<label>{datesErr.dateInputErr}</label>
+								<legend className={styleCreateEvent.legend_detail_form}>
+									Tickets
+								</legend>
+								<input
+									name="tickets"
+									type="number"
+									value={currentDate?.total_tickets || ""}
+									placeholder="Indique numero de tikects..."
+									onChange={handleInputDateChange}
+								/>
+								<label>{datesErr.ticketInputErr}</label>
+
+								<button type="button" title="Agregar detalle" onClick={(e) => { handleAddDateClick(e) }}>
 									+
 								</button>
 							</div>
 
-							<div>
-								{locsAux[currentLocId]?.dates?.map((date: any, id: any) => {
-									console.log(date)
-									return (
-										<fieldset className={styleCreateEvent.legend_form}>
-											<ul>
-												<li key={`${date.price} - ${date.date}`}>
-													<React.Fragment>
-														<p>{`Precio: ${date.price}`} </p>
-														<p>{`Fecha: ${date.date}`} </p>
-													</React.Fragment>
-												</li>
-											</ul>
-											<React.Fragment>
-												<button type="button" key={id} onClick={(e: SyntheticEvent) => removeDateFromLocsAux(id)}>X</button>
-											</React.Fragment>
-										</fieldset>
-									);
-								})}
-							</div>
-							<br />
-							<div className={styleUpdate.container_create}>
-								<button type="button" onClick={(e) => {
-									let select = locSelect.current
-									select.value = 'default'
-									handleConfirm(e)
-									closeModal()
-								}}>
-									Confirmar
-								</button>
-							</div>
-						</DatesModal>
+						</div>
+
+					</fieldset>{' '}
+					<br />
+					<fieldset className={styleCreateEvent.fieldset_form}>
+						<legend className={styleCreateEvent.legend_form}>
+							Localidades agregadas:
+						</legend>
+						{
+							locsForSubmit.length > 0
+								? locsForSubmit
+									.map((loc: any, iLoc: any) => {
+										let locData = locations?.rows?.find((location: any) => location.id === parseInt(loc.id));
+										return locData ?
+											(
+												<div key={iLoc}>
+													<span>{` ${locData.name} `}</span>
+													<span>{` ${locData.address} `}</span>
+													{/* <button onClick={(e) => handleRemoveLoc(e,loc.id)}>X</button> */}
+													<ul>
+														{loc.dates.map((date: any, i: any) => (
+															<li key={i}>{`$${date.price} || ${date.date}`}
+																<button type="button" key={i} onClick={(e: SyntheticEvent) => removeDateFromLocsAux2(i, locData.id)}>X</button>
+															</li>
+														))}
+													</ul>
+												</div>
+											)
+											: null
+									})
+								: <h1>No hay localidades cargadas para el evento</h1>
+						}
+						<label>{err.locationsErr}</label>
 					</fieldset>{' '}
 					{/* EN MODAL */}
 					<button type="submit" className={styleCreateEvent.bottom_form}>
 						Create
 					</button>
+					<button onClick={(e) => { e.preventDefault(); console.log(err) }}>VER ERRORES</button>
+					<button onClick={(e) => {
+						e.preventDefault();
+						const event = {
+							...input,
+							locations: locsForSubmit,
+						}; console.log(event)
+					}}>VER EVENTO</button>
 				</div>
 			</form>
 		</React.Fragment>
--- client/src/components/CreateEvent/useCreateEvent.tsx
+++ client/src/components/CreateEvent/useCreateEvent.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
 
-export const useCreateEvent = ({locations} : any) => {
+export const useCreateEvent = ({ locations }: any) => {
 
-  const initialState : any={
+  const initialState: any = {
     name: '',
     description: '',
     background_image: '',
@@ -12,96 +12,119 @@ export const useCreateEvent = ({locations} : any) => {
   const [input, setInput] = useState(initialState);
   const [currentLocId, setCurrentLocId] = useState(''); // 
   const [currentDate, setCurrentDate] = useState({});
-  const [locsAux, setLocsAux] : any = useState({});
-  const [locsForSubmit, setLocsForSubmit] : any = useState([])
+  const [locsAux, setLocsAux]: any = useState({});
+  const [locsForSubmit, setLocsForSubmit]: any = useState([])
+  let freno = false
 
   useEffect(() => {
     if (locations) {
-      let locObject : any= {};
-      locations?.rows?.forEach((loc: any) => locObject[loc.id]={dates:[]})
+      let locObject: any = {};
+      locations?.rows?.forEach((loc: any) => locObject[loc.id] = { dates: [] })
       setLocsAux(locObject);
     }
-  },[locations])
+  }, [locations])
+
+  useEffect(()=>{
+    handleConfirm()
+    console.log("locsAux en el useEffect")
+    console.log(locsAux.hasOwnProperty("1"))
+    console.log(locsAux)
+    
+  },[locsAux])
 
 
   const isAlreadyAdded = (id: number) => {
     return locsAux[id]?.dates?.length > 0;
   }
 
-  const resetState : any = () => {
+  const resetState: any = () => {
     setInput(initialState);
   }
 
-	const handleInputChange = ({
-		target,
-	}: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = ({
+    target,
+  }: React.ChangeEvent<HTMLInputElement>) => {
     console.log(input)
     console.log(locsAux)
     console.log(locsForSubmit)
-		setInput({ ...input, [target.name]: target.value });
-	};
+    setInput({
+      ...input, [target.name]: target.value.length === 1 && target.value[0] === " " ? "" : target.value.length >= 1 && (target.value[0] === " ") ?
+        target.value.trim()[0].toUpperCase() + target.value.trim().substring(1).trim() : target.value.length === 1 && target.value[0] !== " " ?
+          target.value.toUpperCase() : target.value.length >= 1 && (target.value[0] !== " ") && target.value.substring(target.value.length - 1) !== " " ? target.value.trim()[0].toUpperCase() + target.value.trim().substring(1).trim() : target.value
+    });
+  };
 
-	const handleLocationChange = ({
-		target,
-	}: React.ChangeEvent<HTMLInputElement>) => {
-		setCurrentLocId(target.value);
-	};
+  const handleLocationChange = ({
+    target,
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrentLocId(target.value);
+  };
 
-	const handleCategoryChange = ({
-		target,
-	}: React.ChangeEvent<HTMLInputElement>) => {
+  const handleCategoryChange = ({
+    target,
+  }: React.ChangeEvent<HTMLInputElement>) => {
     console.log(target.checked)
-    if (target.checked){
+    if (target.checked) {
       setInput({
         ...input,
         categories: [...input.categories, parseInt(target.value)],
       });
-    }else{
+    } else {
       setInput({
         ...input,
-        categories: input.categories.filter((c: number) => c!==parseInt(target.value))
+        categories: input.categories.filter((c: number) => c !== parseInt(target.value))
       })
     }
-	};
+  };
 
-	const handleInputDateChange = ({
-		target,
-	}: React.ChangeEvent<HTMLInputElement>) => {
-    if (target?.name === 'price'){
-      setCurrentDate({...currentDate, price: parseInt(target?.value)})
-    }else setCurrentDate({...currentDate, date: target?.value})
+  const handleInputDateChange = ({
+    target,
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    if (target?.name === 'price') {
+      setCurrentDate({ ...currentDate, price: parseInt(target?.value) })
+    }else if(target?.name === 'tickets'){
+      setCurrentDate({ ...currentDate, total_tickets: parseInt(target?.value) })
+    } else setCurrentDate({ ...currentDate, date: target?.value })
     console.log(currentDate)
-	};
+  };
 
-  const handleAddDate = (e : any) => {
+  const handleAddDate = (e: any) => {
     e.preventDefault();
-    if (locsAux[currentLocId]) {
-      setLocsAux((prevState: any) =>{
-        const dates = [...prevState[currentLocId].dates];
-        dates.push(currentDate)
-        return {...prevState, [currentLocId]:{dates}}
-      })
-      console.log('guardado')
-      setCurrentDate({});
-    }
+      if (locsAux[currentLocId]) {
+        setLocsAux((prevState: any) => {
+          const dates = [...prevState[currentLocId].dates];
+          dates.push(currentDate)
+          return { ...prevState, [currentLocId]: { dates } }
+        })
+        console.log('guardado')
+        setCurrentDate({});
+      }
   }
 
-  const handleConfirm = (e : any) => {
-    e.preventDefault();
+  const handleConfirm = (e?: any) => {
+    if (e) e.preventDefault();
     const locs = Object.entries(locsAux)
+    console.log("sssssssssssssssss")
+    console.log(locsAux)
     console.log(locs)
-    let loadedLocs = locs.filter((loc: any) => loc[1].dates.length > 0).map((loc: any) => ({id: loc[0], dates: loc[1].dates}))
+    let loadedLocs = locs.filter((loc: any) => loc[1].dates.length > 0).map((loc: any) => ({ id: loc[0], dates: loc[1].dates }))
     setLocsForSubmit(loadedLocs)
     setCurrentLocId('')
   }
 
-  const handleRemoveLoc = (e: any, id : any) => {
+  const handleConfirm2 = (e: any) => {
     e.preventDefault();
-    setLocsAux((prevState: any) => ({...prevState, [id]:{...prevState[id],dates:[]}}))
+
+    setCurrentLocId('')
+  }
+
+  const handleRemoveLoc = (e: any, id: any) => {
+    e.preventDefault();
+    setLocsAux((prevState: any) => ({ ...prevState, [id]: { ...prevState[id], dates: [] } }))
     setLocsForSubmit((prevState: any) => prevState.filter((loc: any) => loc.id !== id))
   }
 
-  const handleUpdadteFetch = (eventInfo : any) => {
+  const handleUpdadteFetch = (eventInfo: any) => {
     console.log(eventInfo)
     let mappedCategoriesIds = eventInfo?.categories?.map((c: any) => c.id)
     let mappedLocations = eventInfo?.locations?.map((loc: any) => ({
@@ -117,13 +140,15 @@ export const useCreateEvent = ({locations} : any) => {
       categories: mappedCategoriesIds || [],
     })
     mappedLocations.forEach((loc: any) => {
-      setLocsAux((prevState: any) => ({...prevState, [loc.id]:{...prevState[loc.id],dates:loc.dates}}))
+      setLocsAux((prevState: any) => ({ ...prevState, [loc.id]: { ...prevState[loc.id], dates: loc.dates } }))
     })
     setLocsForSubmit(mappedLocations)
   }
 
-  const removeDateFromLocsAux = (id : any) => {
+  const removeDateFromLocsAux = (id: any) => {
+    console.log("datos de eliminacion")
     console.log(id)
+    console.log(currentLocId)
     setLocsAux((prevState: any) => {
       const dates = [...prevState[currentLocId].dates];
       dates.splice(id, 1)
@@ -131,23 +156,33 @@ export const useCreateEvent = ({locations} : any) => {
     })
   }
 
-	return [
-		input,
+  const removeDateFromLocsAux2 = (id: any, idLoc: any) => {
+    console.log(id)
+    setLocsAux((prevState: any) => {
+      const dates = [...prevState[idLoc].dates];
+      dates.splice(id, 1)
+      return { ...prevState, [idLoc]: { dates } }
+    })
+  }
+
+  return [
+    input,
     resetState,
-		handleInputChange,
+    handleInputChange,
     handleInputDateChange,
     currentDate,
     currentLocId,
     locsAux,
     isAlreadyAdded,
     handleAddDate,
-		handleCategoryChange,
-		handleLocationChange,
+    handleCategoryChange,
+    handleLocationChange,
     handleConfirm,
     locsForSubmit,
     handleRemoveLoc,
     handleUpdadteFetch,
     removeDateFromLocsAux,
-    setCurrentLocId
-	];
+    setCurrentLocId,
+    removeDateFromLocsAux2,
+  ];
 };
