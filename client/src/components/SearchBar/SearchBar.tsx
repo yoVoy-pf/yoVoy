f7ece774bd757f5e20f4777131a8afcb0abcd784
shek1ng <sheking142@gmail.com>
2022-06-28 13:47:56 -0300

    feat(client/components/SearchBar.tsx): add autosuggest to searchbar

--- client/src/components/SearchBar/SearchBar.tsx
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -3,14 +3,21 @@ import { AppDispatch } from '../../redux/store/store';
 import { useDispatch } from 'react-redux';
 import { getSearchEvent } from '../../redux/actions/actions-Create';
 import './SearchBar.css';
-import {Toast} from '../../utils/alerts'
+import { Toast } from '../../utils/alerts';
+import { useGetEventsQuery } from '../../slices/app/eventsApiSlice';
 
 type FormElement = React.FormEvent<HTMLFormElement>;
 type Input = React.ChangeEvent<HTMLInputElement>;
 
-const SearchBar = ({searchEventQuery}: any) => {
+const SearchBar = ({ searchEventQuery }: any) => {
 	const dispatch: AppDispatch = useDispatch();
 	const [input, setInput] = useState('');
+	const {
+		data: events,
+		refetch,
+		isFetching,
+	} = useGetEventsQuery({ _: '' }, { refetchOnMountOrArgChange: true });
+	const eventos = events?.rows?.map((e: any) => e.name);
 
 	function onSubmit(e: FormElement) {
 		e.preventDefault();
@@ -22,7 +29,15 @@ const SearchBar = ({searchEventQuery}: any) => {
 			  });
 		setInput('');
 	}
-
+	function onSubmitSelect(e: any) {
+		input
+			? searchEventQuery(input, e)
+			: Toast.fire({
+					title: 'Ingrese el nombre de un Evento',
+					icon: 'warning',
+			  });
+		setInput(e);
+	}
 	function onInputChange(e: Input) {
 		setInput(e.target.value);
 	}
@@ -38,6 +53,30 @@ const SearchBar = ({searchEventQuery}: any) => {
 					placeholder="Buscar..."
 				/>
 				<input className="searchBar-bg-input2" type="submit" value="🔍" />
+				<div className="searchBar-autoSuggest">
+					{input &&
+						eventos
+							?.filter((item: any) => {
+								const searchTerm = input.toLowerCase();
+								const name = item.toLowerCase();
+
+								return (
+									searchTerm &&
+									name.startsWith(searchTerm) &&
+									name !== searchTerm
+								);
+							})
+							.slice(0, 10)
+							.map((e: any) => (
+								<div
+									className="searchBar-autoSuggest-row"
+									onClick={() => onSubmitSelect(e)}
+									key={e}
+								>
+									{e}
+								</div>
+							))}
+				</div>
 			</form>
 		</React.Fragment>
 	);
