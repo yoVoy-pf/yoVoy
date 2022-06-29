07d8e2bb3bd7259bfa6190bc6547e60f6070917a
Federico A. Gomez <fmtgomez@gmail.com>
2022-06-28 21:59:07 -0300

    feat(client/tickets):update style

--- client/src/components/Tickets/Tickets.tsx
+++ client/src/components/Tickets/Tickets.tsx
@@ -11,10 +11,12 @@ const Tickets = ({ ticket }: Props) => {
 		<React.Fragment>
 			<div className={style_ticket.container}>
 				<div className={style_ticket.bg}>
-					<h2>{ticket.event.name}</h2>
-					<h4>Cantidad de Tickets: {ticket.quantity}</h4>
-					<h4>Precio: {'$' + ticket.transaction_amount}</h4>
-					<h4>Estado de la Compra: {ticket.status}</h4>
+					<div className={style_ticket.text}>
+						<h2>{ticket.event.name}</h2>
+						<h4>Cantidad de Tickets: {ticket.quantity}</h4>
+						<h4>Precio: {'$' + ticket.transaction_amount}</h4>
+						<h4>Estado de la Compra: {ticket.status}</h4>
+					</div>
 				</div>
 			</div>
 		</React.Fragment>
